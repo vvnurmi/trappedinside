0d9f5673fdf98544f8d45c0361ba14c7a6f5f3d8
Ville Nurmi <ville.v.nurmi@gmail.com>
2018-11-18 00:09:47 +0200

    Added GenericLevels test project

---
+++ test-projects/GenericLevels/Assets/Scripts/TilemapCollider.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.Tilemaps;
+
+/// <summary>
+/// Collider that covers the painted tiles of a TileMap.
+/// </summary>
+public class TilemapCollider : MonoBehaviour
+{
+    [Tooltip("Create colliders for tiles in this tilemap.")]
+    public Tilemap tilemap;
+
+    private void Start()
+    {
+        Debug.Assert(tilemap != null);
+
+        var tileAnchor = new Vector2(tilemap.tileAnchor.x, tilemap.tileAnchor.y);
+        var bounds = tilemap.cellBounds;
+        Debug.Assert(bounds.zMin == 0 && bounds.zMax == 1);
+        for (int y = bounds.yMin; y < bounds.yMax; y++)
+            for (int x = bounds.xMin; x < bounds.xMax; x++)
+            {
+                var tile = tilemap.GetTile(new Vector3Int(x, y, 0));
+                if (tile == null) continue;
+                var box = gameObject.AddComponent<BoxCollider2D>();
+                box.offset = new Vector2(x, y) + tileAnchor;
+                box.size = Vector2.one;
+            }
+    }
+}
