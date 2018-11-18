e929349107a18b9cc878be631f05407171f513f8
goomba-86 <olli.a.sjoberg@gmail.com>
2018-11-18 11:16:51 +0200

    Add simple camera controller script to Castlevania test project
    Script reads player position and moves the camera based on it

---
+++ test-projects/Castlevania/Assets/Scripts/CameraController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    public GameObject player;
+    public float maxDistanceFromCenter = 1.5f;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        var currentDistance = transform.position.x - player.transform.position.x;
+
+        if(currentDistance > maxDistanceFromCenter) {
+            MoveCamera(player.transform.position.x + maxDistanceFromCenter);
+        } else if (currentDistance < -maxDistanceFromCenter) {
+            MoveCamera(player.transform.position.x - maxDistanceFromCenter);
+        }
+
+
+	}
+
+    private void MoveCamera(float newXCoordinate) {
+        transform.position = new Vector3(newXCoordinate, transform.position.y, transform.position.z);
+    }
+
+}
