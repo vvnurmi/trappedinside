7590e726bdd41c63758664734b5acac797394429
Ville Nurmi <ville.v.nurmi@gmail.com>
2018-11-19 00:09:50 +0200

    Added a shoddily scrolling background with FreeParallax

---
+++ test-projects/GenericLevels/Assets/Scripts/ScrollingParallax.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(FreeParallax))]
+public class ScrollingParallax : MonoBehaviour
+{
+    public Transform follow;
+    public float SpeedMultiplier = 0.5f;
+
+    private Vector3 previousPos;
+    private FreeParallax parallax;
+
+    private void Start()
+    {
+        parallax = GetComponent<FreeParallax>();
+    }
+
+    private void Update()
+    {
+        var movement = follow.position - previousPos;
+        previousPos = follow.position;
+        parallax.Speed = SpeedMultiplier * movement.x / Time.deltaTime;
+    }
+}
