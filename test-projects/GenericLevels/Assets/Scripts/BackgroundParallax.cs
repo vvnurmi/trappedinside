b2bbb04a8f2725526a61809f4f6689fe2e8da968
Ville Nurmi <ville.v.nurmi@gmail.com>
2018-11-21 23:21:51 +0200

    Camera is limited horizontally to hand-picked room boundaries, and parallax is a bit bigger to cover bigger screens

--- test-projects/GenericLevels/Assets/Scripts/BackgroundParallax.cs
+++ test-projects/GenericLevels/Assets/Scripts/BackgroundParallax.cs
@@ -5,7 +5,7 @@ public class BackgroundParallax : MonoBehaviour
     [Tooltip("The parallax will follow this object's movement.")]
     public GameObject follow;
     [Tooltip("Bigger scale makes the parallax move slower.")]
-    public float scale = 100;
+    public float speedScale = 100;
     [Tooltip("A game object with a Quad component. Because lazy programmer didn't do it in code.")]
     public GameObject quadTemplate;
     [Tooltip("Material containing the parallax texture. Lazy programmer didn't do it in code.")]
@@ -28,7 +28,7 @@ public class BackgroundParallax : MonoBehaviour
 
     private void Update()
     {
-        var pos = follow != null ? follow.transform.position / scale : Vector3.zero;
+        var pos = follow != null ? follow.transform.position / speedScale : Vector3.zero;
         mesh.mesh.uv = new[]
         {
             new Vector2(pos.x + 0, pos.y + 0),
---
+++ test-projects/GenericLevels/Assets/Scripts/CameraFollowPlayer.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Camera))]
+public class CameraFollowPlayer : MonoBehaviour
+{
+    private new Camera camera;
+    private Player player;
+    private float[] cameraBounds;
+
+    private void Start()
+    {
+        camera = GetComponent<Camera>();
+        player = FindObjectOfType<Player>();
+
+        var cameraBoundRoot = GameObject.FindGameObjectWithTag("CameraBounds");
+        var boundNum = cameraBoundRoot.transform.childCount;
+        cameraBounds = new float[boundNum];
+        for (int i = 0; i < boundNum; i++)
+            cameraBounds[i] = cameraBoundRoot.transform.GetChild(i).transform.position.x;
+    }
+
+    private void LateUpdate()
+    {
+        FollowPlayer();
+        ObeyBounds();
+    }
+
+    private void FollowPlayer()
+    {
+        var cameraZ = camera.transform.position.z;
+        var playerPos = player.transform.position;
+        camera.transform.position = new Vector3(
+            playerPos.x,
+            playerPos.y,
+            cameraZ);
+    }
+
+    private void ObeyBounds()
+    {
+        // Figure out extreme world coordinates of the camera.
+        var vertExtent = camera.orthographicSize;
+        var horzExtent = vertExtent * Screen.width / Screen.height;
+        var cameraPos = camera.transform.position;
+        var cameraLeft = cameraPos.x - horzExtent;
+        var cameraRight = cameraPos.x + horzExtent;
+
+        foreach (var boundX in cameraBounds)
+        {
+            // Limit camera from the right.
+            if (cameraRight > boundX && cameraPos.x < boundX)
+                cameraPos.x = boundX - horzExtent;
+
+            // Limit camera from the left.
+            if (cameraLeft < boundX && cameraPos.x >= boundX)
+                cameraPos.x = boundX + horzExtent;
+        }
+
+        // Actuate new camera position.
+        camera.transform.position = cameraPos;
+    }
+}
