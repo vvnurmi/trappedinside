ba016b97210327c8c9fdaa322fb78021e93e01dc
Ville Nurmi <ville.v.nurmi@gmail.com>
2018-11-25 12:58:47 +0200

    Simple dialog box appears when player enters the house for the first time

---
+++ test-projects/GenericLevels/Assets/Scripts/GameState.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public static class GameState
+{
+    private static GameObject liveDialog;
+
+    /// <summary>
+    /// Pauses the game, displays a dialog, and waits for player acknowledgement.
+    /// </summary>
+    public static void ShowDialog(GameObject dialog, Vector2 dialogPosition)
+    {
+        //Time.timeScale = 0;
+        var dialogPos = new Vector3(dialogPosition.x, dialogPosition.y, 0);
+        liveDialog = Object.Instantiate(dialog, dialogPos, Quaternion.identity);
+    }
+
+    public static void HideDialog()
+    {
+        if (liveDialog == null) return;
+
+        Object.Destroy(liveDialog);
+        liveDialog = null;
+        //Time.timeScale = 1;
+    }
+}
