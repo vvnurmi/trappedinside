e52db42b9de844a2cdc44844f2e2269a0ced035a
Ville Nurmi <ville.v.nurmi@gmail.com>
2018-12-02 21:34:45 +0200

    Test scene Playground and initial PlayerController script

---
+++ test-projects/SuperReplicaBros/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [Tooltip("Maximum walking speed")]
+    public float maxSpeed = 5;
+
+    private void Start()
+    {
+
+    }
+
+    private void Update()
+    {
+        var horizontalInput = Input.GetAxis("Horizontal");
+        var oldPos = gameObject.transform.position;
+        var deltaPos = Vector3.right * horizontalInput * maxSpeed * Time.deltaTime;
+        gameObject.transform.position = oldPos + deltaPos;
+    }
+}
