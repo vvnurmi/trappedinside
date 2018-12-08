e75ccca3a287bca075fd6d877a80689fc117cdd6
Ville Nurmi <ville.v.nurmi@gmail.com>
2018-12-08 20:23:24 +0200

    Display level title screen for 2 seconds and then start proper gameplay

--- test-projects/SuperReplicaBros/Assets/Scripts/MenuLogic.cs
+++ test-projects/SuperReplicaBros/Assets/Scripts/MenuLogic.cs
@@ -1,61 +1,81 @@
 ﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+enum MenuMode
+{
+    Title,
+    Gameplay,
+}
 
 public class MenuLogic : MonoBehaviour
 {
-    [Tooltip("The player to enable/disable based on menus.")]
-    public GameObject player;
-
-    new private PlayerCamera camera;
+    private MenuMode mode = MenuMode.Title;
 
     private void Start()
     {
-        Debug.Assert(player != null);
-        camera = GetComponentInParent<PlayerCamera>();
-        Debug.Assert(camera != null);
+        if (DieForUniquePersistence()) return;
 
-        ShowMenu("title");
+        SceneManager.LoadScene("Game Title", LoadSceneMode.Additive);
         HidePlayer();
     }
 
     private void Update()
     {
         var isStartPressed = Input.GetButtonDown("Jump");
-        if (isStartPressed)
-        {
-            HideMenu();
-            ShowPlayer();
-        }
+        if (mode == MenuMode.Title && isStartPressed)
+            BeginGameplay();
     }
 
     public void HidePlayer()
     {
-        camera.player = null;
-        player.SetActive(false);
+        FindCamera().player = null;
+        FindPlayer().SetActive(false);
     }
 
-    public void ShowPlayer()
+    private void BeginGameplay()
     {
-        camera.player = player;
-        player.SetActive(true);
+        mode = MenuMode.Gameplay;
+        SceneManager.LoadScene("Level Title");
+        Invoke("LoadLevel", time: 2.0f);
     }
 
-    public void HideMenu()
+    private void LoadLevel()
     {
-        foreach (Transform child in transform)
-        {
-            var menu = child.gameObject;
-            menu.SetActive(false);
-        }
+        SceneManager.LoadScene("Level 1");
     }
 
-    public void ShowMenu(string name)
+    private PlayerCamera FindCamera()
     {
-        var child = transform.Find(name);
-        Debug.Assert(child != null);
-        var mode = child.gameObject;
-        if (mode.activeSelf) return;
+        var cameraObject = GameObject.FindGameObjectWithTag("MainCamera");
+        Debug.Assert(cameraObject != null);
+        var camera = cameraObject.GetComponent<PlayerCamera>();
+        Debug.Assert(camera != null);
+        return camera;
+    }
 
-        HideMenu();
-        mode.SetActive(true);
+    private GameObject FindPlayer()
+    {
+        var player = GameObject.FindGameObjectWithTag("Player");
+        Debug.Assert(player != null);
+        return player;
+    }
+
+    /// <summary>
+    /// Ensures that only one object with this component exists, and the object
+    /// lives over scene transitions. Returns true if we're a duplicate that
+    /// will die immediately.
+    /// </summary>
+    private bool DieForUniquePersistence()
+    {
+        // Make object not get destroyed when a new scene is loaded.
+        DontDestroyOnLoad(this);
+
+        // If one of us already exists, don't let any more in.
+        var others = FindObjectsOfType<MenuLogic>();
+        Debug.Assert(others.Length >= 1);
+        if (others.Length == 1) return false;
+
+        Destroy(this);
+        return true;
     }
 }
---
+++ test-projects/SuperReplicaBros/Assets/Scripts/StartGame.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartGame : MonoBehaviour
+{
+    private void Start()
+    {
+        Invoke("LoadLevel", time: 2.0f);
+    }
+
+    private void LoadLevel()
+    {
+        SceneManager.LoadScene("Level 1");
+    }
+}
