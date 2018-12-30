4b9c1941f59c9e2104746018f45e0f56f1961285
Ville Nurmi <ville.v.nurmi@gmail.com>
2018-12-30 14:46:15 +0200

    Cloud tilemap in the background that scrolls on its own

---
+++ test-projects/SuperReplicaBros/Assets/Scripts/Extensions/TilemapExtensions.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public static class TilemapExtensions
+{
+    public static BoundsInt GetTightCellBounds(this Tilemap tilemap)
+    {
+        var looseBounds = tilemap.cellBounds;
+        var min = new Vector3Int(int.MaxValue, int.MaxValue, int.MaxValue);
+        var max = new Vector3Int(int.MinValue, int.MinValue, int.MinValue);
+        for (int z = looseBounds.zMin; z < looseBounds.zMax; z++)
+            for (int y = looseBounds.yMin; y < looseBounds.yMax; y++)
+                for (int x = looseBounds.xMin; x < looseBounds.xMax; x++)
+                    if (tilemap.GetTile(new Vector3Int(x, y, z)) != null)
+                    {
+                        min = Vector3Int.Min(min, new Vector3Int(x, y, z));
+                        max = Vector3Int.Max(max, new Vector3Int(x, y, z));
+                    }
+        return new BoundsInt(
+            xMin: min.x,
+            yMin: min.y,
+            zMin: min.z,
+            sizeX: max.x - min.x + 1,
+            sizeY: max.y - min.y + 1,
+            sizeZ: max.z - min.z + 1);
+    }
+}
---
+++ test-projects/SuperReplicaBros/Assets/Scripts/ScrollingSky.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.Tilemaps;
+
+[RequireComponent(typeof(Tilemap))]
+public class ScrollingSky : MonoBehaviour
+{
+    [Tooltip("How fast the sky moves on its own.")]
+    public float windSpeed = 1.0f;
+
+    private GameObject gameObjectClone;
+
+    private void Start()
+    {
+        CloneTilemap();
+    }
+
+    private void Update()
+    {
+        var windMoveX = Time.deltaTime * windSpeed;
+        var totalMove = Vector3.right * windMoveX;
+        gameObject.transform.position += totalMove;
+        gameObjectClone.transform.position += totalMove;
+    }
+
+    private void CloneTilemap()
+    {
+        var tilemap = gameObject.GetComponent<Tilemap>();
+        tilemap.ResizeBounds();
+        var bounds = tilemap.GetTightCellBounds();
+        gameObjectClone = Instantiate(gameObject, transform.parent);
+        // This script controls also the cloned object, so it doesn't need its copy of the script.
+        Destroy(gameObjectClone.GetComponent<ScrollingSky>());
+        gameObjectClone.transform.position += new Vector3(bounds.xMax - bounds.xMin, 0, 0);
+    }
+}
