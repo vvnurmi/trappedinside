f2be90975232b3344feeee77c5e8a968b8511a0d
Ville Nurmi <ville.v.nurmi@gmail.com>
2018-12-30 15:13:17 +0200

    Sky tilemap loops indefinitely

--- test-projects/SuperReplicaBros/Assets/Scripts/ScrollingSky.cs
+++ test-projects/SuperReplicaBros/Assets/Scripts/ScrollingSky.cs
@@ -15,14 +15,34 @@ public class ScrollingSky : MonoBehaviour
 
     private Vector3 oldCameraPosition;
     private GameObject gameObjectClone;
+    private Tilemap tilemap;
+    private BoundsInt tilemapBounds;
+    private Camera cameraComponent;
 
     private void Start()
     {
         CloneTilemap();
         oldCameraPosition = camera.transform.position;
+        cameraComponent = camera.GetComponent<Camera>();
     }
 
     private void Update()
+    {
+        ScrollTilemap();
+        EnsureTilemapsOnScreen();
+    }
+
+    private void CloneTilemap()
+    {
+        tilemap = gameObject.GetComponent<Tilemap>();
+        tilemapBounds = tilemap.GetTightCellBounds();
+        gameObjectClone = Instantiate(gameObject, transform.parent);
+        // This script controls also the cloned object, so it doesn't need its copy of the script.
+        Destroy(gameObjectClone.GetComponent<ScrollingSky>());
+        gameObjectClone.transform.position += new Vector3(tilemapBounds.xMax - tilemapBounds.xMin, 0, 0);
+    }
+
+    private void ScrollTilemap()
     {
         var windMoveX = Time.deltaTime * windSpeed;
         var newCameraPosition = camera.transform.position;
@@ -34,14 +54,18 @@ public class ScrollingSky : MonoBehaviour
         oldCameraPosition = newCameraPosition;
     }
 
-    private void CloneTilemap()
+    private void EnsureTilemapsOnScreen()
     {
-        var tilemap = gameObject.GetComponent<Tilemap>();
-        tilemap.ResizeBounds();
-        var bounds = tilemap.GetTightCellBounds();
-        gameObjectClone = Instantiate(gameObject, transform.parent);
-        // This script controls also the cloned object, so it doesn't need its copy of the script.
-        Destroy(gameObjectClone.GetComponent<ScrollingSky>());
-        gameObjectClone.transform.position += new Vector3(bounds.xMax - bounds.xMin, 0, 0);
+        // Note: 'tilemap' belongs to 'gameObject' which is always drawn left of 'gameObjectClone'.
+        var cameraArea = cameraComponent.GetWorldArea();
+        var tilemapMin = tilemap.CellToWorld(tilemapBounds.min);
+        var tilemapMax = tilemap.CellToWorld(tilemapBounds.max);
+        var moveDirection =
+            cameraArea.xMin < tilemapMin.x ? -1.0f :
+            tilemapMax.x < cameraArea.xMin ? +1.0f :
+            0.0f;
+        var move = moveDirection * Vector3.right * (tilemapMax.x - tilemapMin.x);
+        gameObject.transform.position += move;
+        gameObjectClone.transform.position += move;
     }
 }
