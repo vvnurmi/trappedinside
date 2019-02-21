80efe8060b8b3d4ab8c8aa6ce38743b1af4d9a24
Ville Nurmi <ville.v.nurmi@gmail.com>
2019-02-21 23:02:31 +0200

    UIController preparation for insertion into any and all scenes for easier game debuggability

--- TrappedInside/Assets/Scripts/UIController.cs
+++ TrappedInside/Assets/Scripts/UIController.cs
@@ -1,15 +1,48 @@
 ﻿using UnityEngine;
 using UnityEngine.SceneManagement;
 
+public enum UIMode
+{
+    Title,
+    Gameplay,
+}
+
 public class UIController : MonoBehaviour
 {
+    private UIMode mode = UIMode.Title;
+
+    private void OnEnable()
+    {
+        if (Lifetime.KillForUniquePersistence<UIController>(gameObject))
+            return;
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    private void OnDisable()
+    {
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+    }
+
+    private void OnSceneLoaded(Scene scene, LoadSceneMode sceneMode)
+    {
+        // A simple heuristic to determine if we loaded a gameplay or title scene.
+        if (scene.name == "Title")
+            mode = UIMode.Title;
+        else if (scene.name.StartsWith("Level"))
+            mode = UIMode.Gameplay;
+        else
+            Debug.Assert(false, "Scene name heuristic failed");
+    }
+
     private void FixedUpdate()
     {
-        bool promptPressed =
-            Input.GetButtonDown("Fire1") ||
-            Input.GetButtonDown("Jump");
-
-        if (promptPressed)
-            SceneManager.LoadScene("Level1");
+        if (mode == UIMode.Title)
+        {
+            bool promptPressed =
+                Input.GetButtonDown("Fire1") ||
+                Input.GetButtonDown("Jump");
+            if (promptPressed)
+                SceneManager.LoadScene("Level1");
+        }
     }
 }
