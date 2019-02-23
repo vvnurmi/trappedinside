8de264e8f41d0d36426591bb36f09d3b83727c82
goomba-86 <olli.a.sjoberg@gmail.com>
2019-02-23 20:53:58 +0200

    Add test course

---
+++ TrappedInside/Assets/Maps/grass-land-3.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.1" name="grass-land-3" tilewidth="16" tileheight="16" tilecount="377" columns="29">
+ <image source="grass-land-3.png" trans="ff00ff" width="464" height="208"/>
+</tileset>
