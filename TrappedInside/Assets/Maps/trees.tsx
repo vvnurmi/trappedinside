979d45d5efccf08b0f8d142a2afdbd2a11bdb4f1
goomba-86 <olli.a.sjoberg@gmail.com>
2019-03-09 20:03:54 +0200

    Add trees to level 1

---
+++ TrappedInside/Assets/Maps/trees.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.1" name="trees" tilewidth="16" tileheight="16" tilecount="600" columns="40">
+ <image source="trees.png" trans="ff00ff" width="640" height="240"/>
+</tileset>
