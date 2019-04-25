87041bdb7921a64dfc588f4c355509edc69b3a7d
goomba-86 <olli.a.sjoberg@gmail.com>
2019-04-25 19:19:32 +0300

    Add waterfall splash

--- TrappedInside/Assets/Maps/waterfall.tsx
+++ TrappedInside/Assets/Maps/waterfall.tsx
@@ -1,52 +1,76 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.2.1" name="waterfall" tilewidth="16" tileheight="16" tilecount="24" columns="6">
- <image source="waterfall.png" trans="ff00ff" width="96" height="64"/>
+<tileset version="1.2" tiledversion="1.2.1" name="waterfall" tilewidth="16" tileheight="16" tilecount="45" columns="9">
+ <image source="waterfall.png" trans="ff00ff" width="144" height="80"/>
  <tile id="0">
   <animation>
    <frame tileid="0" duration="100"/>
-   <frame tileid="6" duration="100"/>
-   <frame tileid="12" duration="100"/>
+   <frame tileid="9" duration="100"/>
    <frame tileid="18" duration="100"/>
+   <frame tileid="27" duration="100"/>
   </animation>
  </tile>
  <tile id="1">
   <animation>
    <frame tileid="1" duration="100"/>
-   <frame tileid="7" duration="100"/>
-   <frame tileid="13" duration="100"/>
+   <frame tileid="10" duration="100"/>
    <frame tileid="19" duration="100"/>
+   <frame tileid="28" duration="100"/>
   </animation>
  </tile>
  <tile id="2">
   <animation>
    <frame tileid="2" duration="100"/>
-   <frame tileid="8" duration="100"/>
-   <frame tileid="14" duration="100"/>
+   <frame tileid="11" duration="100"/>
    <frame tileid="20" duration="100"/>
+   <frame tileid="29" duration="100"/>
   </animation>
  </tile>
  <tile id="3">
   <animation>
    <frame tileid="3" duration="100"/>
-   <frame tileid="9" duration="100"/>
-   <frame tileid="15" duration="100"/>
+   <frame tileid="12" duration="100"/>
    <frame tileid="21" duration="100"/>
+   <frame tileid="30" duration="100"/>
   </animation>
  </tile>
  <tile id="4">
   <animation>
    <frame tileid="4" duration="100"/>
-   <frame tileid="10" duration="100"/>
-   <frame tileid="16" duration="100"/>
+   <frame tileid="13" duration="100"/>
    <frame tileid="22" duration="100"/>
+   <frame tileid="31" duration="100"/>
   </animation>
  </tile>
  <tile id="5">
   <animation>
    <frame tileid="5" duration="100"/>
-   <frame tileid="11" duration="100"/>
-   <frame tileid="17" duration="100"/>
+   <frame tileid="14" duration="100"/>
    <frame tileid="23" duration="100"/>
+   <frame tileid="32" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="6">
+  <animation>
+   <frame tileid="6" duration="100"/>
+   <frame tileid="15" duration="100"/>
+   <frame tileid="24" duration="100"/>
+   <frame tileid="33" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="7">
+  <animation>
+   <frame tileid="7" duration="100"/>
+   <frame tileid="16" duration="100"/>
+   <frame tileid="25" duration="100"/>
+   <frame tileid="34" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="8">
+  <animation>
+   <frame tileid="8" duration="100"/>
+   <frame tileid="17" duration="100"/>
+   <frame tileid="26" duration="100"/>
+   <frame tileid="35" duration="100"/>
   </animation>
  </tile>
 </tileset>
