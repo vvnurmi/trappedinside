ce8c17fb8b8627216c84a30914871be99c440e93
goomba-86 <olli.a.sjoberg@gmail.com>
2019-04-26 14:49:43 +0300

    Add torches

--- TrappedInside/Assets/Maps/stone-land.tsx
+++ TrappedInside/Assets/Maps/stone-land.tsx
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.2.1" name="stone-land" tilewidth="16" tileheight="16" tilecount="66" columns="11">
- <image source="stone-land.png" trans="ff00ff" width="176" height="96"/>
+<tileset version="1.2" tiledversion="1.2.1" name="stone-land" tilewidth="16" tileheight="16" tilecount="78" columns="13">
+ <image source="stone-land.png" trans="ff00ff" width="208" height="96"/>
  <tile id="0">
   <objectgroup draworder="index">
    <object id="1" x="0" y="0" width="16" height="16"/>
@@ -22,14 +22,11 @@
   </objectgroup>
  </tile>
  <tile id="11">
-  <objectgroup draworder="index">
-   <object id="1" x="0" y="0" width="16" height="16"/>
-  </objectgroup>
- </tile>
- <tile id="12">
-  <objectgroup draworder="index">
-   <object id="1" x="0" y="0" width="16" height="16"/>
-  </objectgroup>
+  <animation>
+   <frame tileid="11" duration="100"/>
+   <frame tileid="37" duration="100"/>
+   <frame tileid="63" duration="100"/>
+  </animation>
  </tile>
  <tile id="13">
   <objectgroup draworder="index">
@@ -41,27 +38,44 @@
    <object id="1" x="0" y="0" width="16" height="16"/>
   </objectgroup>
  </tile>
- <tile id="22">
+ <tile id="15">
   <objectgroup draworder="index">
    <object id="1" x="0" y="0" width="16" height="16"/>
   </objectgroup>
  </tile>
- <tile id="23">
+ <tile id="16">
   <objectgroup draworder="index">
-   <object id="1" x="0" y="0" width="16" height="6"/>
+   <object id="1" x="0" y="0" width="16" height="16"/>
   </objectgroup>
  </tile>
  <tile id="24">
-  <objectgroup draworder="index">
-   <object id="1" x="0" y="0" width="16" height="6"/>
-  </objectgroup>
- </tile>
- <tile id="25">
-  <objectgroup draworder="index">
-   <object id="1" x="0" y="0" width="16" height="6"/>
-  </objectgroup>
+  <animation>
+   <frame tileid="24" duration="100"/>
+   <frame tileid="50" duration="100"/>
+   <frame tileid="76" duration="100"/>
+  </animation>
  </tile>
  <tile id="26">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="27">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="16" height="6"/>
+  </objectgroup>
+ </tile>
+ <tile id="28">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="16" height="6"/>
+  </objectgroup>
+ </tile>
+ <tile id="29">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="16" height="6"/>
+  </objectgroup>
+ </tile>
+ <tile id="30">
   <objectgroup draworder="index">
    <object id="1" x="0" y="0" width="16" height="16"/>
   </objectgroup>
