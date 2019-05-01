2567b16e872a44dcd8e7eb6bb553e718bca98cf9
Ville Nurmi <ville.v.nurmi@gmail.com>
2019-05-01 22:41:25 +0300

    Simple implementation of Level 1 start sequence

---
+++ TrappedInside/Assets/Scripts/UI/NarrativeTypist.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// Displays text in a text box as if it was being typed in.
+/// </summary>
+public class NarrativeTypist : MonoBehaviour
+{
+    [Tooltip("How many characters to type per second")]
+    public float charsPerSecond = 10;
+
+    private Text textComponent;
+    private string fullText;
+    private float startTime;
+
+    #region MonoBehaviour overrides
+
+    private void Awake()
+    {
+        textComponent = GetComponentInChildren<Text>();
+        Debug.Assert(textComponent != null);
+        fullText = textComponent.text;
+    }
+
+    private void OnEnable()
+    {
+        startTime = Time.time;
+    }
+
+    private void FixedUpdate()
+    {
+        var charsToShow = Mathf.Min(
+            fullText.Length,
+            Mathf.RoundToInt((Time.time - startTime) * charsPerSecond));
+        textComponent.text = fullText.Substring(0, charsToShow);
+
+        // If all of the text is revealed, go to sleep as there's nothing more to do.
+        if (charsToShow == fullText.Length)
+            gameObject.SetActive(false);
+    }
+
+    #endregion
+}
---
+++ TrappedInside/Assets/Scripts/UI/TimelineSuspender.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.Playables;
+
+/// <summary>
+/// Suspends a timeline while the gameObject is active.
+/// </summary>
+public class TimelineSuspender : MonoBehaviour
+{
+    public PlayableDirector timeline;
+
+    /// <summary>
+    /// Safety time, in seconds, after resume when we won't pause the timeline again.
+    /// This is a workaround to how Timeline likes to play the paused frame
+    /// again after resume, resulting in an endless pause-resume loop.
+    /// </summary>
+    private const float ResumeClearance = 1;
+
+    private double timelinePauseTime;
+    private float resumeTime = float.MinValue;
+
+    private void OnEnable()
+    {
+        if (timeline != null && Time.time > resumeTime + ResumeClearance)
+            timelinePauseTime = timeline.time;
+    }
+
+    private void Update()
+    {
+        // Hack: Keep timeline's time in place. This is better than calling
+        // timeline.Pause() because pause will revert all values that the timeline
+        // has modified back to their original values.
+        timeline.time = timelinePauseTime;
+    }
+
+    private void OnDisable()
+    {
+        if (timeline != null)
+            resumeTime = Time.time;
+    }
+}
