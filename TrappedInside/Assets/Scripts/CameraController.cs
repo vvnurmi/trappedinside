ac00588cce33f23c5e1f862db519809d007b5bf7
goomba-86 <olli.a.sjoberg@gmail.com>
2019-05-01 21:00:30 +0300

    First version of camera controller

--- TrappedInside/Assets/Scripts/CameraController.cs
+++ TrappedInside/Assets/Scripts/CameraController.cs
@@ -2,53 +2,39 @@
 
 public class CameraController : MonoBehaviour
 {
-
     public GameObject mike;
-
-    [Tooltip("Camera area limits in world coordinates.")]
-    public Rect cameraAreaLimits;
-
-    [Tooltip("Player moving area in viewport coordinates.")]
-    public Rect playerMovingArea;
-
     private new Camera camera;
 
-    private Vector3 offset;
+    public float leftBound = 0;
+    public float rightBound = 1000;
+    public float lowerBound = -100;
+    public float upperBound = 0;
 
-    private Vector3 BoundlessPosition => mike.transform.position + offset;
+    private float cameraWidth;
+    private float cameraHeight;
 
-    //private Vector3 BoundedPosition => new Vector3(
-    //    x: GetBoundedValue(BoundlessPosition.x, cameraAreaLimits.xMin, cameraAreaLimits.xMax),
-    //    y: GetBoundedValue(BoundlessPosition.y, cameraAreaLimits.yMin, cameraAreaLimits.yMax),
-    //    z: -1);
+    private Vector3 BoundlessPosition => new Vector3(mike.transform.position.x, mike.transform.position.y, transform.position.z);
+
+    private Vector3 BoundedPosition => new Vector3(
+        x: GetBoundedValue(BoundlessPosition.x, leftBound + cameraWidth, rightBound - cameraWidth),
+        y: GetBoundedValue(BoundlessPosition.y, lowerBound + cameraHeight, upperBound - cameraHeight),
+        z: -1);
 
     private float GetBoundedValue(float value, float min, float max)
     {
         return value < min ? min : Mathf.Min(value, max);
     }
 
-    // Start is called before the first frame update
     void Start()
     {
-        offset = transform.position - mike.transform.position;
         camera = GetComponent<Camera>();
+        var bottomLeftCorner = camera.ViewportToWorldPoint(new Vector3(0f, 0f));
+        cameraWidth = transform.position.x - bottomLeftCorner.x;
+        cameraHeight = transform.position.y - bottomLeftCorner.y;
     }
 
-    // Update is called once per frame
     void LateUpdate()
     {
-        transform.position = mike.transform.position + offset;
-        //var viewPortLocation = camera.WorldToViewportPoint(mike.transform.position);
-        //if (playerMovingArea.Contains(viewPortLocation))
-        //{
-        //    return;
-        //}
-
-        //var viewportOffsetInWorldCoordinates = 
-        //    camera.ViewportToWorldPoint(new Vector3(playerMovingArea.width / 2.0f, playerMovingArea.height / 2.0f, 0));
-
-        //transform.position = BoundedPosition - new Vector3(viewportOffsetInWorldCoordinates.x, 0);
+        transform.position = BoundedPosition;
     }
-
-
 }
