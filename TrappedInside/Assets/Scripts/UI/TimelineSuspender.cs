92d6d60a3ce1e3096c42efe0da5947db14a71174
Ville Nurmi <ville.v.nurmi@gmail.com>
2019-05-06 23:39:39 +0300

    Extracted TimelineSuspenderSettings so that the same timeline reference doesn't need to be copied to every suspender

--- TrappedInside/Assets/Scripts/UI/TimelineSuspender.cs
+++ TrappedInside/Assets/Scripts/UI/TimelineSuspender.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using UnityEngine.Playables;
 
 /// <summary>
@@ -6,8 +7,6 @@ using UnityEngine.Playables;
 /// </summary>
 public class TimelineSuspender : MonoBehaviour
 {
-    public PlayableDirector timeline;
-
     /// <summary>
     /// Safety time, in seconds, after resume when we won't pause the timeline again.
     /// This is a workaround to how Timeline likes to play the paused frame
@@ -15,13 +14,28 @@ public class TimelineSuspender : MonoBehaviour
     /// </summary>
     private const float ResumeClearance = 1;
 
+    // Set about once, probably in Start().
+    private Lazy<TimelineSuspenderSettings> settings;
+
+    // Modified during gameplay.
     private double timelinePauseTime;
     private float resumeTime = float.MinValue;
 
+    public TimelineSuspender()
+    {
+        settings = new Lazy<TimelineSuspenderSettings>(() =>
+        {
+            var settings = GetComponentInParent<TimelineSuspenderSettings>();
+            Debug.Assert(settings != null,
+                $"Expected to find {nameof(TimelineSuspenderSettings)} from the parent of {nameof(TimelineSuspender)}");
+            return settings;
+        });
+    }
+
     private void OnEnable()
     {
-        if (timeline != null && Time.time > resumeTime + ResumeClearance)
-            timelinePauseTime = timeline.time;
+        if (settings.Value.timeline != null && Time.time > resumeTime + ResumeClearance)
+            timelinePauseTime = settings.Value.timeline.time;
     }
 
     private void Update()
@@ -29,12 +43,12 @@ public class TimelineSuspender : MonoBehaviour
         // Hack: Keep timeline's time in place. This is better than calling
         // timeline.Pause() because pause will revert all values that the timeline
         // has modified back to their original values.
-        timeline.time = timelinePauseTime;
+        settings.Value.timeline.time = timelinePauseTime;
     }
 
     private void OnDisable()
     {
-        if (timeline != null)
+        if (settings.Value.timeline != null)
             resumeTime = Time.time;
     }
 }
---
+++ TrappedInside/Assets/Scripts/UI/TimelineSuspenderSettings.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using UnityEngine.Playables;
+
+public class TimelineSuspenderSettings : MonoBehaviour
+{
+    [Tooltip("Which timeline to suspend when child TimelineSuspender instances are activated.")]
+    public PlayableDirector timeline;
+}
