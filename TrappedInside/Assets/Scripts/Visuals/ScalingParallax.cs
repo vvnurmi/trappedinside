35b770712bd09e9d50729e784ca062413bc4f102
Ville Nurmi <ville.v.nurmi@gmail.com>
2019-05-19 18:00:36 +0300

    ScalingParallax can parallax multiple objects

--- TrappedInside/Assets/Scripts/Visuals/ScalingParallax.cs
+++ TrappedInside/Assets/Scripts/Visuals/ScalingParallax.cs
@@ -1,35 +1,50 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 public class ScalingParallax : MonoBehaviour
 {
-    [Tooltip("How much to scale the object. Affects also parallax speed. Scale of 1.0 means do nothing.")]
-    public float scale = 1.0f;
+    [Serializable]
+    public class ObjectScale
+    {
+        public GameObject obj;
+        public float scale = 1.0f;
+    }
 
     [Tooltip("The camera for which to do the parallax scroll.")]
     public GameObject parallaxCamera;
 
-    // Set about once, probably in Start().
-    private Vector3 originalScale;
+    [Tooltip("How much to scale the object. Affects also parallax speed. Scale of 1.0 means do nothing.")]
+    public ObjectScale[] objectScales;
 
     // Modified during gameplay.
     private Vector3 oldCameraPosition;
 
+    #region MonoBehaviour overrides
+
     private void Awake()
     {
         oldCameraPosition = parallaxCamera.transform.position;
-        originalScale = transform.localScale;
-        transform.localScale = new Vector3(
-            originalScale.x * scale,
-            originalScale.y,
-            originalScale.z);
+        foreach (var x in objectScales)
+            x.obj.transform.localScale = new Vector3(
+                x.obj.transform.localScale.x * x.scale,
+                x.obj.transform.localScale.y,
+                x.obj.transform.localScale.z);
     }
 
     private void LateUpdate()
     {
         var newCameraPosition = parallaxCamera.transform.position;
-        var parallaxMoveX = -(newCameraPosition - oldCameraPosition).x * (scale - 1.0f);
-        var totalMove = Vector3.right * parallaxMoveX;
-        gameObject.transform.position += totalMove;
+        foreach (var x in objectScales)
+            ParallaxScroll(x, oldCameraPosition, newCameraPosition);
         oldCameraPosition = newCameraPosition;
+    }
+
+    #endregion
+
+    private static void ParallaxScroll(ObjectScale x, Vector3 oldCameraPosition, Vector3 newCameraPosition)
+    {
+        var parallaxMoveX = -(newCameraPosition - oldCameraPosition).x * (x.scale - 1.0f);
+        var totalMove = Vector3.right * parallaxMoveX;
+        x.obj.transform.position += totalMove;
     }
 }
