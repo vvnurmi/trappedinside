41fc67e300d1a6394da5ea3855fc64e2950cf97b
Ville Nurmi <ville.v.nurmi@gmail.com>
2019-05-19 20:30:57 +0300

    ScalingParallaxEditor lets you choose the parallax scale for the farthest and nearest tilemap layers

--- TrappedInside/Assets/Scripts/Editor/ScalingParallaxEditor.cs
+++ TrappedInside/Assets/Scripts/Editor/ScalingParallaxEditor.cs
@@ -1,44 +1,58 @@
-﻿using UnityEngine;
-using System.Linq;
+﻿using System.Linq;
 using UnityEditor;
+using UnityEngine;
 using UnityEngine.Tilemaps;
-
 using ObjScale = ScalingParallax.ObjectScale;
 
 [CustomEditor(typeof(ScalingParallax))]
 public class ScalingParallaxEditor : Editor
 {
+    private const float MinScale = 0.01f;
+    private const float MaxScale = 10.0f;
+
+    private float farScale = 0.7f;
+    private float nearScale = 1.1f;
+
     public override void OnInspectorGUI()
     {
         DrawDefaultInspector();
 
-        if (GUILayout.Button("Populate scales from child tilemap renderers"))
-        {
-            ScalingParallax scalingParallax = (ScalingParallax)target;
+        farScale = ScaleField("Far Scale", farScale);
+        nearScale = ScaleField("Near Scale", nearScale);
 
-            var children = scalingParallax.transform.Cast<Transform>();
-            var scaleData = (
-                from child in children
-                let obj = child.gameObject
-                let renderer = obj.GetComponent<TilemapRenderer>()
-                where renderer != null
-                let order = renderer.sortingOrder
-                orderby order ascending
-                select (obj, order))
-                .ToArray();
-            if (scaleData.Length > 0)
-            {
-                var minOrder = scaleData[0].order;
-                var maxOrder = scaleData[scaleData.Length - 1].order;
-                var deepScale = 0.7f;
-                var shallowScale = 1.0f;
-                scalingParallax.objectScales = (
-                    from x in scaleData
-                    let normalizedOrder = (x.order - minOrder) / (float)(maxOrder - minOrder)
-                    let scale = deepScale + (shallowScale - deepScale) * normalizedOrder
-                    select new ObjScale { obj = x.obj, scale = scale })
-                    .ToArray();
-            }
-        }
+        if (GUILayout.Button("Populate scales from child tilemap renderers"))
+            PopulateScales();
+    }
+
+    private float ScaleField(string name, float oldScale)
+    {
+        var newScale = EditorGUILayout.FloatField("Far Scale", oldScale);
+        return Mathf.Clamp(newScale, MinScale, MaxScale);
+    }
+
+    private void PopulateScales()
+    {
+        ScalingParallax scalingParallax = (ScalingParallax)target;
+
+        var children = scalingParallax.transform.Cast<Transform>();
+        var scaleData = (
+            from child in children
+            let obj = child.gameObject
+            let renderer = obj.GetComponent<TilemapRenderer>()
+            where renderer != null
+            let order = renderer.sortingOrder
+            orderby order ascending
+            select (obj, order))
+            .ToArray();
+        if (scaleData.Length == 0) return;
+
+        var minOrder = scaleData[0].order;
+        var maxOrder = scaleData[scaleData.Length - 1].order;
+        scalingParallax.objectScales = (
+            from x in scaleData
+            let normalizedOrder = (x.order - minOrder) / (float)(maxOrder - minOrder)
+            let scale = farScale + (nearScale - farScale) * normalizedOrder
+            select new ObjScale { obj = x.obj, scale = scale })
+            .ToArray();
     }
 }
