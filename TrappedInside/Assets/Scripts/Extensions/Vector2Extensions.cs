45005c293e25aca2f0f0faffa26d2b782a49b51e
Ville Nurmi <ville.v.nurmi@gmail.com>
2019-06-01 19:51:23 +0300

    Added Vector2Extensions.Distance2 and WithMagnitude

---
+++ TrappedInside/Assets/Scripts/Extensions/Vector2Extensions.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public static class Vector2Extensions
+{
+    public static float Distance2(this Vector2 a, Vector2 b) =>
+        Vector2.SqrMagnitude(b - a);
+
+    public static Vector2 WithMagnitude(this Vector2 vector, float magnitude) =>
+        vector.normalized * magnitude;
+}
