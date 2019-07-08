146f5754a40750db6fd4b52f9920c2f59e2194eb
goomba-86 <olli.a.sjoberg@gmail.com>
2019-07-08 21:30:24 +0300

    Improve mountain layout

--- TrappedInside/Assets/Maps/grass-land-3.tsx
+++ TrappedInside/Assets/Maps/grass-land-3.tsx
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.2.1" name="grass-land-3" tilewidth="16" tileheight="16" tilecount="435" columns="29">
- <image source="grass-land-3.png" trans="ff00ff" width="464" height="240"/>
+<tileset version="1.2" tiledversion="1.2.1" name="grass-land-3" tilewidth="16" tileheight="16" tilecount="580" columns="29">
+ <image source="grass-land-3.png" trans="ff00ff" width="464" height="320"/>
  <tile id="0">
   <objectgroup draworder="index">
    <object id="8" x="0" y="0" width="16" height="16"/>
