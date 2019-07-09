648108735284bf4ace1da8d80ca29220f26aa675
Ville Nurmi <ville.v.nurmi@gmail.com>
2019-07-10 00:12:22 +0300

    Moved damage and dying handling from CharacterController2D to HitPoints

--- TrappedInside/Assets/Scripts/Characters/HitPoints.cs
+++ TrappedInside/Assets/Scripts/Characters/HitPoints.cs
@@ -1,4 +1,14 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+/// <summary>
+/// Interface for reacting to receiving damage.
+/// </summary>
+public interface IDamaged
+{
+    void OnDamaged();
+}
 
 /// <summary>
 /// Interface for custom death handler.
@@ -15,17 +25,38 @@ public class HitPoints : MonoBehaviour
 {
     [Tooltip("Number of hit points when not damaged.")]
     public int maxHitPoints = 1;
+
+    [Tooltip("Seconds after hit until another hit is possible.")]
+    public float hitDelay = 1.0f;
+
     public int CurrentHitPoints { get; private set; }
+
+    // Set about once, probably in Start().
+    private Animator animator; // May be null.
+
+    // Modified during gameplay.
+    private float nextHitAllowedAt = 0f;
+
+    #region MonoBehaviour overrides
 
     private void Start()
     {
+        animator = GetComponentInChildren<Animator>();
         CurrentHitPoints = maxHitPoints;
     }
+
+    #endregion
 
     public void Damage(int damage)
     {
         Debug.Assert(damage > 0);
+        if (CurrentHitPoints == 0) return;
+        if (Time.time < nextHitAllowedAt) return;
+
+        nextHitAllowedAt = Time.time + hitDelay;
+
         CurrentHitPoints = Mathf.Max(0, CurrentHitPoints - damage);
+        CallHandlersOrDefault((Action<IDamaged>)(a => a.OnDamaged()), DefaultDamagedHandler);
         if (CurrentHitPoints == 0)
             Die();
     }
@@ -34,12 +65,26 @@ public class HitPoints : MonoBehaviour
     {
         // We're dead. Call death handlers if there's any.
         // If there's no handlers then just destroy the game object.
+        CallHandlersOrDefault<IDying>(a => a.OnDying(), DefaultDyingHandler);
+    }
 
-        var handlers = GetComponents<IDying>();
+    private void DefaultDamagedHandler()
+    {
+        animator?.Play("Damage");
+    }
+
+    private void DefaultDyingHandler()
+    {
+        animator?.Play("Death");
+    }
+
+    private void CallHandlersOrDefault<TInterface>(Action<TInterface> invoke, Action defaultt)
+    {
+        var handlers = GetComponents<TInterface>();
         if (handlers.Length > 0)
             foreach (var handler in handlers)
-                handler.OnDying();
+                invoke(handler);
         else
-            Destroy(gameObject);
+            defaultt();
     }
 }
