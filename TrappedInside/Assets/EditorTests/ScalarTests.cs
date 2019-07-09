ccce5c393df4c115856fcc9d1d06b3e61482b6b4
Ville Nurmi <ville.v.nurmi@gmail.com>
2019-07-09 18:04:25 +0300

    Added Scalar.LerpTowards with unit tests

---
+++ TrappedInside/Assets/EditorTests/ScalarTests.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+
+namespace Tests
+{
+    public class ScalarTests
+    {
+        [Test]
+        public void LerpTowards()
+        {
+            Assert.AreEqual(0, Scalar.LerpTowards(0, 0, 0));
+            Assert.AreEqual(0, Scalar.LerpTowards(0, 1, 0));
+            Assert.AreEqual(0, Scalar.LerpTowards(0, 0, 1));
+            Assert.AreEqual(1, Scalar.LerpTowards(0, 1, 1));
+            Assert.AreEqual(1, Scalar.LerpTowards(1, 0, 0));
+            Assert.AreEqual(0, Scalar.LerpTowards(1, 0, 1));
+            Assert.AreEqual(0.3f, Scalar.LerpTowards(0.2f, 0.5f, 0.1f));
+            Assert.AreEqual(0.5f, Scalar.LerpTowards(0.2f, 0.5f, 0.4f));
+            Assert.AreEqual(0.7f, Scalar.LerpTowards(0.8f, 0.5f, 0.1f));
+            Assert.AreEqual(0.5f, Scalar.LerpTowards(0.8f, 0.5f, 0.4f));
+        }
+    }
+}
---
+++ TrappedInside/Assets/Scripts/Math/Scalar.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public static class Scalar
+{
+    /// <summary>
+    /// Lerps from <paramref name="value"/> towards <paramref name="target"/>
+    /// at most <paramref name="step"/> amount.
+    /// </summary>
+    public static float LerpTowards(float value, float target, float step)
+    {
+        return Mathf.Abs(target - value) <= step
+            ? target
+            : value + step * Mathf.Sign(target - value);
+    }
+}
