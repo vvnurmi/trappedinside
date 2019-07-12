1b00d409d48f3eb19d27131b419467c58bb6bc6c
Ville Nurmi <ville.v.nurmi@gmail.com>
2019-07-12 16:59:55 +0300

    On Mike's death, display a box with a Mikey quip

--- TrappedInside/Assets/Scripts/Characters/DeathRestartsLevel.cs
+++ TrappedInside/Assets/Scripts/Characters/DeathRestartsLevel.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using UnityEngine;
+using UnityEngine.UI;
 
 /// <summary>
 /// When <see cref="HitPoints"/> signals death, do some UI tricks
@@ -10,6 +11,13 @@ public class DeathRestartsLevel : MonoBehaviour, IDying
     [Tooltip("Restart level after this many seconds have passed since death.")]
     public float secondsUntilLevelRestart = 5;
 
+    [Tooltip("Dialog box for displaying game over text.")]
+    public GameObject gameOverBox;
+
+    [Tooltip("Quips to display in game over box.")]
+    [TextArea]
+    public string[] gameOverQuips;
+
     public void OnDying()
     {
         StartCoroutine(DyingHandler());
@@ -17,7 +25,16 @@ public class DeathRestartsLevel : MonoBehaviour, IDying
 
     private IEnumerator DyingHandler()
     {
+        var canvas = GameObject.FindGameObjectWithTag("Canvas");
+        Debug.Assert(canvas != null);
+        Instantiate(gameOverBox, canvas.transform);
+        var quip = GameObject.FindGameObjectWithTag("GameOverQuip");
+        Debug.Assert(quip != null);
+        var quipText = quip.GetComponent<Text>();
+        quipText.text = gameOverQuips[Random.Range(0, gameOverQuips.Length)];
+
         yield return new WaitForSeconds(secondsUntilLevelRestart);
+
         UIController.Instance.RestartLevel();
     }
 }
