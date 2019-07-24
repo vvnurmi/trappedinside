e8721cc5b371066a41f63c8e35c6f49006f30634
Ville Nurmi <ville.v.nurmi@gmail.com>
2019-07-24 13:07:19 +0300

    Load council scene when jumping up from the mountain in Level 1

---
+++ TrappedInside/Assets/Scripts/UI/LevelLoadOnTriggerEnter.cs
@@ -0,0 +1,13 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+public class LevelLoadOnTriggerEnter : MonoBehaviour
+{
+    [Tooltip("Which level to load.")]
+    public SceneAsset nextLevel;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        UIController.Instance.LoadLevel(nextLevel);
+    }
+}
