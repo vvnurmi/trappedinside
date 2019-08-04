fd9ee4aac1ce009660de4de85d1cbbf2dadf9a87
Ville Nurmi <ville.v.nurmi@gmail.com>
2019-08-04 22:20:40 +0300

    HitPointBarController finds the player by tag "Player"

--- TrappedInside/Assets/Scripts/UI/HitPointBarController.cs
+++ TrappedInside/Assets/Scripts/UI/HitPointBarController.cs
@@ -1,13 +1,26 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 
+/// <summary>
+/// Updates the display of the player's hit points.
+/// </summary>
 public class HitPointBarController : MonoBehaviour
 {
     public Image[] hitPointImages;
     public Sprite fullHeart;
     public Sprite emptyHeart;
-    public HitPoints hitPoints;
-    
+
+    private HitPoints hitPoints;
+
+    private void Awake()
+    {
+        var playerTag = "Player";
+        var player = GameObject.FindWithTag(playerTag);
+        Debug.Assert(player != null, $"{nameof(HitPointBarController)} couldn't find anything tagged {playerTag}");
+        hitPoints = player.GetComponent<HitPoints>();
+        Debug.Assert(hitPoints != null, $"{nameof(HitPointBarController)} couldn't find {nameof(HitPoints)} on the player");
+    }
+
     void Update()
     {
         for(int i = 0; i < hitPointImages.Length; i++)
