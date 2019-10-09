d5471bee63255a891128cb5e4b3aac0042ae5571
Olli <Sjberg>
2019-10-09 21:08:05 +0300

    Move Hornet colliders to sub-objects

--- TrappedInside/Assets/Scripts/Characters/FlyMovement.cs
+++ TrappedInside/Assets/Scripts/Characters/FlyMovement.cs
@@ -3,34 +3,33 @@
 [RequireComponent(typeof(CharacterController2D))]
 public class FlyMovement : MonoBehaviour
 {
-
     [Tooltip("Speed of fly movement, in world units per second.")]
     public float speed = 0.3f;
 
     private CharacterController2D characterController;
-    private GameObject player;
-    private bool followPlayer = false;
     private bool IsFacingRight => characterController.state.collisions.faceDir == 1;
+    private ProximityTrigger proximityTrigger;
+    private GameObject player;
 
     void Start()
     {
         characterController = GetComponent<CharacterController2D>();
+        proximityTrigger = GetComponentInChildren<ProximityTrigger>();
+        player = GameObject.FindWithTag("Player");
         characterController.state.collisions.faceDir = -1;
     }
 
     void FixedUpdate()
     {
-        if (followPlayer)
+        if (proximityTrigger.PlayerInProximity)
         {
-            var ownPosition = transform.position;
-            var playerPosition = player.transform.position;
-            var direction = playerPosition - ownPosition;
+            Debug.Assert(player != null, "Player was null in FlyMovement.cs");
+            var direction = player.transform.position - transform.position;
             var deltaPosition = direction.normalized * speed * Time.deltaTime;
             transform.Translate(deltaPosition, Space.World);
 
-            if ((deltaPosition.x < 0 && IsFacingRight) || (deltaPosition.x > 0 && !IsFacingRight))
+            if ((deltaPosition.x < 0 && IsFacingRight) ||  (deltaPosition.x > 0 && !IsFacingRight))
                 Flip();
-
         }
     }
 
@@ -44,20 +43,4 @@ public class FlyMovement : MonoBehaviour
             transform.localScale.z);
     }
 
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (collision.CompareTag("Player"))
-        {
-            if (player == null)
-                player = collision.gameObject;
-
-            followPlayer = true;
-        }
-    }
-
-    private void OnTriggerExit2D(Collider2D collision)
-    {
-        if (collision.CompareTag("Player"))
-            followPlayer = false;
-    }
 }
---
+++ TrappedInside/Assets/Scripts/Characters/ProximityTrigger.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class ProximityTrigger : MonoBehaviour
+{
+    public bool PlayerInProximity { get; private set; }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (IsPlayer(collision))
+            PlayerInProximity = true;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (IsPlayer(collision))
+            PlayerInProximity = false;
+    }
+
+    private bool IsPlayer(Collider2D collision) => collision.CompareTag("Player");
+}
