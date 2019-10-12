fce3ad30473bac4623a1758534f3a04cacef3c7e
Olli <Sjberg>
2019-10-12 21:53:11 +0300

    Hornet related updates
    * Hornet nest test scene
    * Hornet nest game object
    * Hornet dying animation update

---
+++ TrappedInside/Assets/Scripts/HornetSpawner.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HornetSpawner : MonoBehaviour
+{
+
+    public GameObject hornet;
+    public float instantiateTimeout = 5.0f;
+
+    private float previousInstantiateTime = 0.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time - previousInstantiateTime > instantiateTimeout)
+        {
+            Instantiate(hornet, transform.position + new Vector3(0, -0.5f, 0), Quaternion.identity);
+            previousInstantiateTime = Time.time;
+        }
+    }
+}
