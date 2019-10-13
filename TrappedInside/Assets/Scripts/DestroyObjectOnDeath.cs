7b2d401bb0de5010586aa2e6d10093859540efed
Olli <Sjberg>
2019-10-13 14:34:11 +0300

    Add a script to hornet prefab that destroys the object after hornet is killed

---
+++ TrappedInside/Assets/Scripts/DestroyObjectOnDeath.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(HitPoints))]
+public class DestroyObjectOnDeath : MonoBehaviour, IDying
+{
+    public float delay = 1.0f;
+
+    public void OnDying()
+    {
+        DestroyGameObject(gameObject);
+    }
+
+    private void DestroyGameObject(GameObject gObject)
+    {
+        var parent = gObject.transform.parent;
+        if (parent != null)
+        {
+            DestroyGameObject(parent.gameObject);
+        }
+        else
+        {
+            Destroy(gObject, delay);
+        }
+
+    }
+}
