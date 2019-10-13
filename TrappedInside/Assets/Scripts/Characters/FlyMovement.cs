d946b86e8546501e4cecb6637870d3837e104a70
Olli <Sjberg>
2019-10-13 13:40:52 +0300

    Add random element to hornet movement

--- TrappedInside/Assets/Scripts/Characters/FlyMovement.cs
+++ TrappedInside/Assets/Scripts/Characters/FlyMovement.cs
@@ -4,12 +4,13 @@
 public class FlyMovement : MonoBehaviour
 {
     [Tooltip("Speed of fly movement, in world units per second.")]
-    public float speed = 0.3f;
+    public float speed = 0.5f;
 
     private CharacterController2D characterController;
     private bool IsFacingRight => characterController.state.collisions.faceDir == 1;
     private ProximityTrigger proximityTrigger;
     private GameObject player;
+    private System.Random random = new System.Random();
 
     void Start()
     {
@@ -26,7 +27,8 @@ public class FlyMovement : MonoBehaviour
             Debug.Assert(player != null, "Player was null in FlyMovement.cs");
             var direction = player.transform.position - transform.position;
             var deltaPosition = direction.normalized * speed * Time.deltaTime;
-            transform.Translate(deltaPosition, Space.World);
+            var randomComponent = deltaPosition.magnitude * new Vector3(0, (float)random.NextDouble());
+            transform.Translate(deltaPosition + randomComponent, Space.World);
 
             if ((deltaPosition.x < 0 && IsFacingRight) ||  (deltaPosition.x > 0 && !IsFacingRight))
                 Flip();
