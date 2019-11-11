e2dc0cef3bc3a344f403caf7c86148b63a238dd3
Olli <Sjberg>
2019-11-11 20:23:41 +0200

    Add sample Boss hornet unit test

---
+++ TrappedInside/Assets/EditorTests/BossHornetTests.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+using UnityEngine;
+
+namespace Tests
+{
+    public class BossHornetDataTests
+    {
+
+        [Test]
+        public void TestBossHornetDataCreation()
+        {
+            var bossHornetData = new BossHornetData(
+                hornet: new GameObject("Test"),
+                isFacingLeft: true,
+                flyingPosition: 1);
+
+            Assert.That(bossHornetData.IsFacingLeft, Is.True);
+            Assert.That(bossHornetData.FlyingPosition, Is.EqualTo(1));
+            Assert.That(bossHornetData.ReadyToTransition, Is.False);
+        }
+
+    }
+
+    public class BossHornetStateTests
+    {
+
+    }
+
+}
+
+
