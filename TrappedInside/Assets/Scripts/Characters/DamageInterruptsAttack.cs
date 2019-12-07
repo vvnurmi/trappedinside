f12b33068dabaf55d64ac0731c0c1a65e63fea8f
Ville Nurmi <ville.v.nurmi@gmail.com>
2019-12-07 23:47:46 +0200

    Mike's shielding/attack is interrupted if he's hit

---
+++ TrappedInside/Assets/Scripts/Characters/DamageInterruptsAttack.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// When damaged, any possible attack or shielding will be interrupted.
+/// </summary>
+public class DamageInterruptsAttack : MonoBehaviour, IDamaged
+{
+    // Set about once, probably in Start().
+    private Animator animator;
+    private CharacterController2D characterController;
+
+    // Helpers
+    private TimedAnimationTriggers timedAnimTriggers;
+
+    #region MonoBehaviour overrides
+
+    private void Start()
+    {
+        animator = GetComponentInChildren<Animator>();
+        Debug.Assert(animator != null, $"{nameof(Animator)} not found in children of {name}");
+        characterController = GetComponentInParent<CharacterController2D>();
+        Debug.Assert(characterController != null, $"{nameof(CharacterController2D)} not found in parents of {name}");
+
+        timedAnimTriggers = new TimedAnimationTriggers(animator, 0.1f);
+    }
+
+    private void Update()
+    {
+        timedAnimTriggers.Update();
+    }
+
+    #endregion
+
+    public void OnDamaged()
+    {
+        timedAnimTriggers.Set("StopMelee");
+    }
+}
