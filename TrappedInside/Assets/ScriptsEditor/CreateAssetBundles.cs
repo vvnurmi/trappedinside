26be65ded7a499557bff28762532c75fa039846f
Ville Nurmi <ville.v.nurmi@gmail.com>
2019-12-22 14:17:05 +0200

    Editor extension script to compile asset bundles

---
+++ TrappedInside/Assets/ScriptsEditor/CreateAssetBundles.cs
@@ -0,0 +1,18 @@
+﻿using UnityEditor;
+using System.IO;
+
+// https://docs.unity3d.com/2019.1/Documentation/Manual/AssetBundles-Workflow.html
+
+public class CreateAssetBundles
+{
+    [MenuItem("Assets/Build AssetBundles")]
+    static void BuildAllAssetBundles()
+    {
+        string assetBundleDirectory = "Assets/AssetBundles";
+        if (!Directory.Exists(assetBundleDirectory))
+        {
+            Directory.CreateDirectory(assetBundleDirectory);
+        }
+        BuildPipeline.BuildAssetBundles(assetBundleDirectory, BuildAssetBundleOptions.None, BuildTarget.StandaloneWindows);
+    }
+}
