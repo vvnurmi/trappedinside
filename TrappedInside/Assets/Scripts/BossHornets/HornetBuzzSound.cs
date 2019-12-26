478c516fe8de666e0463ae0f002d0b2feb27d074
Ville Nurmi <ville.v.nurmi@gmail.com>
2019-12-26 21:29:31 +0200

    Boss hornets make a buzzing sound

---
+++ TrappedInside/Assets/Scripts/BossHornets/HornetBuzzSound.cs
@@ -0,0 +1,80 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Handles the details of a hornet's buzzing sound.
+/// </summary>
+[RequireComponent(typeof(AudioSource))]
+public class HornetBuzzSound : MonoBehaviour, IDying
+{
+    [Tooltip("How many seconds until the buzz sound has completely faded in at start.")]
+    public float fadeInSeconds = 1;
+
+    [Tooltip("How many seconds until the buzz sound has completely faded out at death.")]
+    public float fadeOutSeconds = 0.5f;
+
+    [Tooltip("Maximum deviation for the random base pitch multiplier.")]
+    public float basePitchRandom = 0.2f;
+
+    [Tooltip("Maximum deviation for the continuous pitch multiplier.")]
+    public float pitchFluctuationMaxMultiplier = 0.1f;
+
+    [Tooltip("Maximum deviation for the buzz fade out pitch multiplier. 0 = no pitch bend, 1 = bend all the way.")]
+    public float pitchFadeOutMaxMultiplier = 0.2f;
+
+    // Set once, either at start or later.
+    private AudioSource audioSource;
+    private float startTime;
+    private float? deathTime;
+
+    private float GetPitchFadeOut()
+    {
+        if (!deathTime.HasValue) return 1;
+
+        var timeParam = Mathf.InverseLerp(deathTime.Value, deathTime.Value + fadeOutSeconds, Time.time);
+        return Mathf.Lerp(1, 1 - pitchFadeOutMaxMultiplier, timeParam);
+    }
+
+    private float GetVolumeFadeOut()
+    {
+        if (!deathTime.HasValue) return 1;
+
+        var timeParam = Mathf.InverseLerp(deathTime.Value, deathTime.Value + fadeOutSeconds, Time.time);
+        return Mathf.Lerp(1, 0, timeParam);
+    }
+
+    #region MonoBehaviour overrides
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    private void Awake()
+    {
+        startTime = Time.time;
+    }
+
+    private void FixedUpdate()
+    {
+        var randomSeed = Mathf.InverseLerp(int.MinValue, int.MaxValue, GetHashCode());
+
+        var volumeFadeIn = Mathf.InverseLerp(startTime, startTime + fadeInSeconds, Time.time);
+        var finalVolume = volumeFadeIn * GetVolumeFadeOut();
+
+        var basePitch = 1 + basePitchRandom * randomSeed;
+        var pitchFluctuationParam = Mathf.PerlinNoise(Time.time, GetHashCode());
+        var pitchFluctuation = basePitch + pitchFluctuationMaxMultiplier * pitchFluctuationParam;
+        var finalPitch = pitchFluctuation * GetPitchFadeOut();
+
+        audioSource.volume = finalVolume;
+        audioSource.pitch = finalPitch;
+    }
+
+    #endregion
+
+    // IDying interface
+    public void OnDying()
+    {
+        deathTime = Time.time;
+    }
+}
