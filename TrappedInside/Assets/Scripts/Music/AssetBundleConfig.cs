6af421c74082381c94b4a3c12a0c4966ad7f1b25
Ville Nurmi <ville.v.nurmi@gmail.com>
2020-01-02 20:30:05 +0200

    Moved AssetBundleConfig creation to Asset submenu "System"

--- TrappedInside/Assets/Scripts/Music/AssetBundleConfig.cs
+++ TrappedInside/Assets/Scripts/Music/AssetBundleConfig.cs
@@ -1,6 +1,6 @@
 ﻿using UnityEngine;
 
-[CreateAssetMenu(fileName = "AssetBundleConfig", menuName = "AssetBundleConfig")]
+[CreateAssetMenu(fileName = "AssetBundleConfig", menuName = "System/AssetBundleConfig")]
 public class AssetBundleConfig : ScriptableObject
 {
     [Tooltip("Location of the asset bundle, e.g. 'http://foo.com/AssetBundles/music'")]
