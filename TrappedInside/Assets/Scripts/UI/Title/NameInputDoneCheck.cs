aca8bb04b30a010e9a7dc9de3260d9fe2b6678d3
Ville Nurmi <ville.v.nurmi@gmail.com>
2020-01-04 13:06:09 +0200

    Title scene asks for the player's name and then loads the next scene

---
+++ TrappedInside/Assets/Scripts/UI/Title/NameInputDoneCheck.cs
@@ -0,0 +1,35 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+/// <summary>
+/// Handles title screen name input functionality.
+/// CUsed by the name input <see cref="TMP_InputField"/> to signal
+/// <see cref="ActivateChildrenSequentially"/> that the next child
+/// object is to be activated.
+/// </summary>
+public class NameInputDoneCheck : MonoBehaviour, ISequentialChild
+{
+    public bool isDone = false;
+
+    public void OnEndEdit()
+    {
+        isDone = true;
+
+        // Explicitly take focus away from the input field.
+        EventSystem.current.SetSelectedGameObject(null);
+    }
+
+    public bool IsDone() => isDone;
+
+    private void Start()
+    {
+        var inputField = GetComponentInChildren<TMP_InputField>();
+
+        // Hack: Hide caret after input ends. Otherwise it keeps blinking.
+        inputField.onDeselect.AddListener(str => inputField.caretWidth = 0);
+
+        // Help the player by focusing the input field automatically.
+        EventSystem.current.SetSelectedGameObject(inputField.gameObject);
+    }
+}
