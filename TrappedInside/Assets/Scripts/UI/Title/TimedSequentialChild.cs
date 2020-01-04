3a318ee59cbd1ead6ec91ad63879132122400b4b
Ville Nurmi <ville.v.nurmi@gmail.com>
2020-01-04 20:30:24 +0200

    Prettier text positioning in the title scroll

---
+++ TrappedInside/Assets/Scripts/UI/Title/TimedSequentialChild.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// A <see cref="ISequentialChild"/> that is done after a number of seconds.
+/// </summary>
+public class TimedSequentialChild : MonoBehaviour, ISequentialChild
+{
+    public float doneAfterSeconds = 10;
+
+    private float startTime;
+
+    public void Start() => startTime = Time.time;
+    public bool IsDone() => Time.time >= startTime + doneAfterSeconds;
+}
