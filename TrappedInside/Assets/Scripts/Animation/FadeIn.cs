aa6337f2786629bbae694efe542d8d7f31541d12
Ville Nurmi <ville.v.nurmi@gmail.com>
2020-01-06 17:20:42 +0200

    Title stars fade in over 5 seconds each

---
+++ TrappedInside/Assets/Scripts/Animation/FadeIn.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class FadeIn : MonoBehaviour
+{
+    [Tooltip("How long to fade in.")]
+    public float fadeSeconds = 2.0f;
+
+    private float startTime;
+    private SpriteRenderer spriteRenderer;
+
+    private void Start()
+    {
+        startTime = Time.time;
+        spriteRenderer = GetComponent<SpriteRenderer>();
+
+        SetAlpha(0);
+    }
+
+    private void Update()
+    {
+        var alpha = Mathf.InverseLerp(startTime, startTime + fadeSeconds, Time.time);
+        SetAlpha(alpha);
+    }
+
+    private void SetAlpha(float alpha)
+    {
+        var color = spriteRenderer.color;
+        color.a = alpha;
+        spriteRenderer.color = color;
+    }
+}
