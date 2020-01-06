dc4d86bddf148e75158fda64dffb42786765d4a0
Ville Nurmi <ville.v.nurmi@gmail.com>
2020-01-06 16:16:03 +0200

    Added ArrayExtensions.Shuffle

---
+++ TrappedInside/Assets/Scripts/Extensions/ArrayExtensions.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public static class ArrayExtensions
+{
+    /// <summary>
+    /// Fisher-Yates shuffle the elements.
+    /// </summary>
+    public static void Shuffle<T>(this T[] items)
+    {
+        for (int i = 0; i < items.Length - 1; i++)
+        {
+            int j = Random.Range(i, items.Length);
+            T swap = items[i];
+            items[i] = items[j];
+            items[j] = swap;
+        }
+    }
+}
