eb333d95cf5cd743e9c82714f4c906b4207306f7
Ville Nurmi <ville.v.nurmi@gmail.com>
2020-01-06 20:55:30 +0200

    Title name input fades away and final statement is vertically centered

--- TrappedInside/Assets/Scripts/UI/Title/NameInputDoneCheck.cs
+++ TrappedInside/Assets/Scripts/UI/Title/NameInputDoneCheck.cs
@@ -4,23 +4,32 @@ using UnityEngine.EventSystems;
 
 /// <summary>
 /// Handles title screen name input functionality.
-/// CUsed by the name input <see cref="TMP_InputField"/> to signal
+/// Used by the name input <see cref="TMP_InputField"/> to signal
 /// <see cref="ActivateChildrenSequentially"/> that the next child
 /// object is to be activated.
 /// </summary>
-public class NameInputDoneCheck : MonoBehaviour, ISequentialChild
+[RequireComponent(typeof(CanvasRenderer))]
+public class NameInputDoneCheck : MonoBehaviour
 {
-    public bool isDone = false;
+    [Tooltip("How fast to fade out after name input is finished.")]
+    public float fadeOutSeconds = 1.0f;
+
+    private AlphaFader alphaFader;
 
     public void OnEndEdit()
     {
-        isDone = true;
+        // Fade the UI out.
+        alphaFader = new AlphaFader();
+        var renderers = GetComponentsInChildren<CanvasRenderer>();
+        alphaFader.StartFade(fadeOutSeconds, 1, 0, alpha =>
+        {
+            foreach (var renderer in renderers)
+                renderer.SetAlpha(alpha);
+        });
 
         // Explicitly take focus away from the input field.
         EventSystem.current.SetSelectedGameObject(null);
     }
-
-    public bool IsDone() => isDone;
 
     private void Start()
     {
@@ -31,5 +40,18 @@ public class NameInputDoneCheck : MonoBehaviour, ISequentialChild
 
         // Help the player by focusing the input field automatically.
         EventSystem.current.SetSelectedGameObject(inputField.gameObject);
+    }
+
+    private void Update()
+    {
+        // Nothing to do unless we're fading out.
+        if (alphaFader == null) return;
+
+        alphaFader.Update();
+
+        // When we've faded out, deactivate self to signal
+        // ActivateChildrenSequentially to activate the next child.
+        if (alphaFader.IsDone)
+            gameObject.SetActive(false);
     }
 }
