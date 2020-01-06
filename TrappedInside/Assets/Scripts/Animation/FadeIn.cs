651f6cde00eae500c2fcdaf64b798617405b8456
Ville Nurmi <ville.v.nurmi@gmail.com>
2020-01-06 19:09:44 +0200

    Refactored AlphaFader out of FadeIn

--- TrappedInside/Assets/Scripts/Animation/FadeIn.cs
+++ TrappedInside/Assets/Scripts/Animation/FadeIn.cs
@@ -6,27 +6,22 @@ public class FadeIn : MonoBehaviour
     [Tooltip("How long to fade in.")]
     public float fadeSeconds = 2.0f;
 
-    private float startTime;
-    private SpriteRenderer spriteRenderer;
+    private AlphaFader alphaFader = new AlphaFader();
 
     private void Start()
     {
-        startTime = Time.time;
-        spriteRenderer = GetComponent<SpriteRenderer>();
-
-        SetAlpha(0);
+        var spriteRenderer = GetComponent<SpriteRenderer>();
+        alphaFader.StartFade(fadeSeconds, 0, 1, alpha =>
+        {
+            var color = spriteRenderer.color;
+            color.a = alpha;
+            spriteRenderer.color = color;
+        });
     }
 
     private void Update()
     {
-        var alpha = Mathf.InverseLerp(startTime, startTime + fadeSeconds, Time.time);
-        SetAlpha(alpha);
-    }
-
-    private void SetAlpha(float alpha)
-    {
-        var color = spriteRenderer.color;
-        color.a = alpha;
-        spriteRenderer.color = color;
+        if (!alphaFader.IsDone)
+            alphaFader.Update();
     }
 }
---
+++ TrappedInside/Assets/Scripts/Tools/AlphaFader.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using UnityEngine;
+
+/// <summary>
+/// Helper to do an alpha fade in or out.
+/// </summary>
+public class AlphaFader
+{
+    private float startTime;
+    private float endTime;
+    private float beginAlpha;
+    private float endAlpha;
+    private Action<float> setAlpha;
+
+    public bool IsDone { get; private set; }
+
+    /// <summary>
+    /// Initialize a fade. Call <see cref="Update"/> regularly after this.
+    /// </summary>
+    public void StartFade(
+        float fadeSeconds,
+        float beginAlpha,
+        float endAlpha,
+        Action<float> setAlpha)
+    {
+        IsDone = false;
+
+        startTime = Time.time;
+        endTime = startTime + fadeSeconds;
+        this.beginAlpha = beginAlpha;
+        this.endAlpha = endAlpha;
+        this.setAlpha = setAlpha;
+
+        setAlpha(beginAlpha);
+    }
+
+    /// <summary>
+    /// To be called regularly to make the fade happen.
+    /// </summary>
+    public void Update()
+    {
+        var lerpParam = Mathf.InverseLerp(startTime, endTime, Time.time);
+        var alpha = Mathf.Lerp(beginAlpha, endAlpha, lerpParam);
+        setAlpha(alpha);
+
+        if (lerpParam == 1.0f)
+            IsDone = true;
+    }
+}
