0c432def5fa33caed1d22ec1412d78e424fd8712
Ville Nurmi <ville.v.nurmi@gmail.com>
2020-01-12 13:02:52 +0200

    Added EnumFlagAttribute and EnumFlagDrawer to support flag enum editing in the inspector

---
+++ TrappedInside/Assets/Scripts/Tools/EnumFlagAttribute.cs
@@ -0,0 +1,19 @@
+﻿// From https://wiki.unity3d.com/index.php/EnumFlagPropertyDrawer
+
+using UnityEngine;
+
+/// <summary>
+/// Flags an flag enum field as being editable as a bit field instead of
+/// a regular single-valued enum.
+/// </summary>
+public class EnumFlagAttribute : PropertyAttribute
+{
+    public string enumName;
+
+    public EnumFlagAttribute() { }
+
+    public EnumFlagAttribute(string name)
+    {
+        enumName = name;
+    }
+}
