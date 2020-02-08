fc086829e33892670c6336aae05c8267bbf0b465
Olli <Sjberg>
2020-02-08 14:54:21 +0200

    Hedgehog initial commit

---
+++ TrappedInside/Assets/Scripts/Characters/DropIndicator.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class DropIndicator : MonoBehaviour
+{
+
+    BoxCollider2D groudCollider;
+
+    void Start()
+    {
+        groudCollider = GetComponent<BoxCollider2D>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (IsGroundLayer(collision))
+            IsGroundAhead = true;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (IsGroundLayer(collision))
+            IsGroundAhead = false;
+    }
+
+    private bool IsGroundLayer(Collider2D collision) => 
+        LayerMask.LayerToName(collision.gameObject.layer) == "Ground";
+
+
+    public bool IsGroundAhead { get; private set; } = false;
+}
---
+++ TrappedInside/Assets/Scripts/Characters/HedgehogMovement.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class HedgehogMovement : MonoBehaviour
+{
+
+    [Tooltip("Ground collision settings.")]
+    public RaycastColliderConfig groundColliderConfig;
+
+    [Tooltip("Speed of movement along a surface, in world units per second.")]
+    public float walkingSpeed = -0.1f;
+
+    public float gravity = -5.0f;
+
+    private CharacterState characterState;
+    private RaycastCollider groundCollider;
+    private HitPoints hitPoints;
+    private Animator animator;
+    private Vector2 velocity;
+    private DropIndicator dropIndicator;
+
+    private void Start()
+    {
+
+        characterState = GetComponent<CharacterState>();
+        var boxCollider = GetComponent<BoxCollider2D>();
+        dropIndicator = GetComponentInChildren<DropIndicator>();
+        groundCollider = new RaycastCollider(
+                groundColliderConfig,
+                boxCollider,
+                characterState.collisions);
+        hitPoints = GetComponent<HitPoints>();
+        animator = GetComponent<Animator>();
+        velocity = Vector2.zero;
+    }
+
+    void FixedUpdate()
+    {
+        if (velocity.x == 0)
+        {
+            velocity.x = walkingSpeed;
+        }
+
+        velocity.y += gravity * Time.deltaTime;
+        Move(velocity * Time.deltaTime);
+
+        var collisions = characterState.collisions;
+        if (collisions.HasVerticalCollisions)
+        {
+            velocity.y = 0;
+        }
+
+        if (collisions.HasHorizontalCollisions || !dropIndicator.IsGroundAhead)
+        {
+            velocity.x *= -1;
+            transform.localScale = new Vector3(
+                -transform.localScale.x,
+                transform.localScale.y,
+                transform.localScale.z);
+        }
+    }
+
+    private void Move(Vector2 moveAmount)
+    {
+        groundCollider.UpdateRaycastOrigins();
+
+        CollisionInfo collisions = characterState.collisions;
+        collisions.Reset();
+
+        groundCollider.HorizontalCollisions(ref moveAmount);
+        if (moveAmount.y != 0)
+            groundCollider.VerticalCollisions(ref moveAmount);
+
+        transform.Translate(moveAmount);
+    }
+
+}
