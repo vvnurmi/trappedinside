5d22ecd33c6fc03469d34cbde9a53c51b2c22888
Olli <Sjberg>
2020-02-16 21:53:12 +0200

    Spider inital commit

---
+++ TrappedInside/Assets/Scripts/Characters/ContactTrigger.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class ContactTrigger : MonoBehaviour
+{
+    public bool Contact { get; private set; }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Contact = true;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        Contact = false;
+    }
+}
---
+++ TrappedInside/Assets/Scripts/Characters/SpiderMovement.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class SpiderMovement : MonoBehaviour
+{
+
+    enum State { ReadyForAttack, GettingUp, Attacking }
+
+    public float attackSpeed = 2.0f;
+    public float returnSpeed = 0.5f;
+
+    private AttackTrigger attackTrigger;
+    private ContactTrigger contactTrigger;
+    private Animator animator;
+    private State state = State.ReadyForAttack;
+    private Vector2 startingPosition;
+
+    private static readonly string moving = "Moving";
+    private static readonly string still = "Still";
+    private readonly List<string> animatorStates = new List<string> { moving, still };
+
+
+    void Start()
+    {
+        attackTrigger = GetComponentInChildren<AttackTrigger>();
+        contactTrigger = GetComponentInChildren<ContactTrigger>();
+        animator = GetComponent<Animator>();
+        startingPosition = transform.position;
+    }
+
+    void FixedUpdate()
+    {
+        if (state == State.ReadyForAttack)
+        {
+            if (attackTrigger.PlayerInAttackRange)
+            {
+                SetAnimatorState(moving);
+                state = State.Attacking;
+            }
+        }
+        else if (state == State.Attacking)
+        {
+            transform.Translate(new Vector3(0, -attackSpeed * Time.deltaTime));
+            if (contactTrigger.Contact)
+            {
+                state = State.GettingUp;
+            }
+        }
+        else if (state == State.GettingUp)
+        {
+            transform.Translate(new Vector3(0, returnSpeed * Time.deltaTime));
+            if (Vector2.Distance(transform.position, startingPosition) < 0.01)
+            {
+                state = State.ReadyForAttack;
+                SetAnimatorState(still);
+            }
+        }
+    }
+
+
+    private void SetAnimatorState(string state)
+    {
+        ClearAnimatorState();
+        animator.SetBool(state, true);
+    }
+
+    private void ClearAnimatorState()
+    {
+        foreach (var animatorState in animatorStates)
+            animator.SetBool(animatorState, false);
+    }
+
+
+}
