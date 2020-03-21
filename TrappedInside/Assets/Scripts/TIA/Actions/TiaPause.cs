8959a62d127ba5419d182ed2217289d43dfdca84
Ville Nurmi <ville.v.nurmi@gmail.com>
2020-03-21 19:52:12 +0200

    TiaPause uses ITiaActionContext instead of loose arguments

--- TrappedInside/Assets/Scripts/TIA/Actions/TiaPause.cs
+++ TrappedInside/Assets/Scripts/TIA/Actions/TiaPause.cs
@@ -3,7 +3,7 @@
 /// <summary>
 /// Waits for given time until continuing to the next action.
 /// </summary>
-public class TiaPause : ITiaAction
+public class TiaPause : ITiaAction, ITiaActionNew
 {
     [YamlDotNet.Serialization.YamlMember(Alias = "Seconds")]
     public float DurationSeconds { get; set; }
@@ -14,10 +14,20 @@ public class TiaPause : ITiaAction
 
     public void Start(GameObject tiaRoot)
     {
-        finishTime = Time.time + DurationSeconds;
+        throw new System.NotImplementedException();
     }
 
     public void Update(TiaActor actor)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void Start(ITiaActionContext context)
+    {
+        finishTime = Time.time + DurationSeconds;
+    }
+
+    public void Update(ITiaActionContext context)
     {
         if (IsDone) return;
 
