e9b2c41b796fd1a51130e85d0336c3076cc65ff1
Ville Nurmi <ville.v.nurmi@gmail.com>
2020-04-13 22:58:13 +0300

    TIA scripts are addressed by their asset paths relative to "TiaScripts" folder.
    Now TiaScript.Name is redundant, but at least there's no need to duplicate the name
    to both the asset and into the script.

--- TrappedInside/Assets/Scripts/TIA/TiaScriptManager.cs
+++ TrappedInside/Assets/Scripts/TIA/TiaScriptManager.cs
@@ -3,31 +3,28 @@ using System.Linq;
 using System.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.ResourceLocations;
 
 /// <summary>
 /// Handles access to <see cref="TiaScript"/> instances.
+/// 
+/// The name of a TIA script is its relative asset path in the folder that's
+/// labeled in Addressable Assets as "TiaScript".
 /// </summary>
 public class TiaScriptManager : MonoBehaviour
 {
     private static GameObject host;
 
     private Task<IList<TextAsset>> loadTask;
+    private Task<IList<IResourceLocation>> locationTask;
     private Dictionary<string, string> scriptYamls; // TIA script name => TIA script as YAML
 
     public static TiaScriptManager Instance => host.GetComponent<TiaScriptManager>();
 
     public async Task<TiaScript> Get(string name)
     {
-        await loadTask;
-        if (loadTask.Status != TaskStatus.RanToCompletion)
-        {
-            Debug.LogWarning($"TIA script loading is {loadTask.Status}");
+        if (!await TryEnsureScriptsLoaded())
             return TiaScript.Empty;
-        }
-
-        scriptYamls = loadTask.Result.ToDictionary(
-            textAsset => TiaScript.Read(textAsset.text).ScriptName,
-            textAsset => textAsset.text);
 
         if (scriptYamls.TryGetValue(name, out string scriptYaml))
             return GetFromYaml(scriptYaml);
@@ -42,6 +39,53 @@ public class TiaScriptManager : MonoBehaviour
         return script;
     }
 
+    /// <summary>
+    /// Awaits until scripts have been loaded.
+    /// Returns true on success and false on failure. 
+    /// </summary>
+    private async Task<bool> TryEnsureScriptsLoaded()
+    {
+        if (scriptYamls != null) return true;
+
+        await locationTask;
+        if (locationTask.Status != TaskStatus.RanToCompletion)
+        {
+            Debug.LogWarning($"TIA script discovery is {locationTask.Status}");
+            return false;
+        }
+        var locations = locationTask.Result;
+
+        await loadTask;
+        if (loadTask.Status != TaskStatus.RanToCompletion)
+        {
+            Debug.LogWarning($"TIA script loading is {loadTask.Status}");
+            return false;
+        }
+        scriptYamls = locations
+            .Zip(loadTask.Result, (location, textAsset) => (location, textAsset))
+            .ToDictionary<(IResourceLocation, TextAsset), string, string>(
+                x => GetScriptName(x.Item1),
+                x => x.Item2.text);
+
+        // Remove this debug logging later when there's more scripts and TIA feels more stable.
+        foreach (var x in scriptYamls)
+            Debug.Log($"Found TIA script '{x.Key}'");
+
+        return true;
+    }
+
+    private string GetScriptName(IResourceLocation resourceLocation)
+    {
+        // ResourceLocation will be something like "Assets/Data/TiaScripts/MyFolder/Test1.yaml"
+        // and we'll shorten it to "MyFolder/Test1"
+        var path = resourceLocation.PrimaryKey;
+        var pathPrefix = "Assets/Data/TiaScripts/";
+        var pathSuffix = ".yaml";
+        Debug.Assert(path.StartsWith(pathPrefix) && path.EndsWith(pathSuffix),
+            $"Script path is not of expected form '{pathPrefix}XXX{pathSuffix}': '{path}'");
+        return path.Substring(pathPrefix.Length, path.Length - pathPrefix.Length - pathSuffix.Length);
+    }
+
     #region MonoBehaviour overrides
 
     [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
@@ -54,6 +98,7 @@ public class TiaScriptManager : MonoBehaviour
 
     private void Awake()
     {
+        locationTask = Addressables.LoadResourceLocationsAsync("TiaScript", typeof(TextAsset)).Task;
         var asyncOperation = Addressables.LoadAssetsAsync<TextAsset>("TiaScript", null);
         loadTask = asyncOperation.Task;
     }
