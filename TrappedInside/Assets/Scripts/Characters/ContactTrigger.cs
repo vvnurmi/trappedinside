f0f0a9af8e1e69307acb7058ec3394cbf2f3b30a
Olli <Sjberg>
2020-06-07 13:05:53 +0300

    Add hedgehog-spider-test test scenes. Modify spider detect are wider.

--- TrappedInside/Assets/Scripts/Characters/ContactTrigger.cs
+++ TrappedInside/Assets/Scripts/Characters/ContactTrigger.cs
@@ -2,15 +2,21 @@
 
 public class ContactTrigger : MonoBehaviour
 {
+    public bool ignoreDefaultLayer = false;
+
     public bool Contact { get; private set; }
+
+    private int DefaultLayer => LayerMask.NameToLayer("Default");
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        Contact = true;
+        if (collision.gameObject.layer != DefaultLayer)
+            Contact = true;
     }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
-        Contact = false;
+        if (collision.gameObject.layer != DefaultLayer)
+            Contact = false;
     }
 }
