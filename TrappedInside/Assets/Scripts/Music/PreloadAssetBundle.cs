3199ca3e0b414e20e8005a3958c71f2e5c301e68
Ville Nurmi <ville.v.nurmi@gmail.com>
2020-08-04 15:18:39 +0300

    Init scene preloads music asset bundle

---
+++ TrappedInside/Assets/Scripts/Music/PreloadAssetBundle.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class PreloadAssetBundle : MonoBehaviour, ISequentialChild
+{
+    [Tooltip("Asset bundle to preload.")]
+    public AssetBundleConfig assetBundle;
+
+    private bool isDone;
+
+    public bool IsDone() => isDone;
+
+    private IEnumerator Start()
+    {
+        var enumerable = PlayMusicFromAssetBundle.EnsureAssetBundleIsCached(assetBundle.uri);
+        yield return StartCoroutine(enumerable.GetEnumerator());
+        isDone = true;
+    }
+}
