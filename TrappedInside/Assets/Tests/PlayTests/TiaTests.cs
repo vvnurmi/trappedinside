b9237389e57dce44e4edbaddcfe81b647f1189f2
Ville Nurmi <ville.v.nurmi@gmail.com>
2020-08-17 13:59:51 +0300

    Added TiaMove.FlipLeft to mirror the object when it's moving left

--- TrappedInside/Assets/Tests/PlayTests/TiaTests.cs
+++ TrappedInside/Assets/Tests/PlayTests/TiaTests.cs
@@ -202,6 +202,39 @@ namespace Tests
         }
 
         [UnityTest]
+        public IEnumerator MoveLeftFlipsActor()
+        {
+            var tiaRoot = NewGameObject("TIA root");
+
+            var testObject = NewGameObject("test object");
+            testObject.transform.parent = tiaRoot.transform;
+
+            var curveObject = NewGameObject("Test Curve");
+            curveObject.transform.parent = tiaRoot.transform;
+            var curve = curveObject.AddComponent<BezierCurve>();
+            curve.AddPointAt(new Vector3(0, 0));
+            curve.AddPointAt(new Vector3(10, 10));
+            curve.AddPointAt(new Vector3(0, 20));
+
+            var tiaPlayer = tiaRoot.AddComponent<TiaPlayer>();
+            tiaPlayer.script = NewSimpleScript(testObject,
+                new TiaMove
+                {
+                    DurationSeconds = 2,
+                    CurveName = curveObject.name,
+                    FlipLeft = true,
+                });
+
+            const float Epsilon = 1e-4f;
+
+            yield return new WaitForSeconds(0.5f);
+            Assert.AreEqual(1, testObject.transform.localScale.x, Epsilon);
+
+            yield return new WaitForSeconds(1);
+            Assert.AreEqual(-1, testObject.transform.localScale.x, Epsilon);
+        }
+
+        [UnityTest]
         public IEnumerator Animation()
         {
             const string DefaultStateName = "state1";
