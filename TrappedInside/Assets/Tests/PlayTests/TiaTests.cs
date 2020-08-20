48a9c971cbb1960b7895fc0d960473e10236f92d
Ville Nurmi <ville.v.nurmi@gmail.com>
2020-08-20 19:16:06 +0300

    Extracted class TiaBasicActionTests

--- TrappedInside/Assets/Tests/PlayTests/TiaTests.cs
+++ TrappedInside/Assets/Tests/PlayTests/TiaTests.cs
@@ -3,7 +3,6 @@ using System;
 using System.Collections;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
-using UnityEditor.Animations;
 using UnityEngine;
 using UnityEngine.TestTools;
 
@@ -11,32 +10,6 @@ namespace Tests
 {
     public class TiaTests : TiaTestBase
     {
-        [UnityTest]
-        public IEnumerator ActivationAndPause()
-        {
-            var tiaRoot = NewGameObject("TIA root");
-            var testObject = NewGameObject("test object");
-            testObject.transform.parent = tiaRoot.transform;
-
-            var tiaPlayer = tiaRoot.AddComponent<TiaPlayer>();
-            tiaPlayer.script = NewSimpleScript(testObject,
-                new TiaPause { DurationSeconds = 1 },
-                new TiaActivate(),
-                new TiaPause { DurationSeconds = 1 },
-                new TiaDeactivate());
-
-            testObject.SetActive(false);
-
-            yield return new WaitForSeconds(0.5f);
-            Assert.IsFalse(testObject.activeSelf);
-
-            yield return new WaitForSeconds(1);
-            Assert.IsTrue(testObject.activeSelf);
-
-            yield return new WaitForSeconds(1);
-            Assert.IsFalse(testObject.activeSelf);
-        }
-
         [UnityTest]
         public IEnumerator SimultaneousActors()
         {
@@ -96,51 +69,6 @@ namespace Tests
 
             LogAssert.Expect(LogType.Assert, new Regex($"{nameof(TiaActor)} couldn't find '{fakeObject.name}' under .*"));
             yield return new WaitForSeconds(0.5f);
-        }
-
-        [UnityTest]
-        public IEnumerator Animation()
-        {
-            const string DefaultStateName = "state1";
-            const string AnotherStateName = "state2";
-
-            var tiaRoot = NewGameObject("TIA root");
-            var testObject = NewGameObject("test object");
-            testObject.transform.parent = tiaRoot.transform;
-            var animator = testObject.AddComponent<Animator>();
-            var animatorController = new AnimatorController();
-            var defaultAnimatorState = new ChildAnimatorState { state = new AnimatorState { name = DefaultStateName } };
-            var anotherAnimatorState = new ChildAnimatorState { state = new AnimatorState { name = AnotherStateName } };
-            var animatorControllerLayer = new AnimatorControllerLayer
-            {
-                name = "Base Layer",
-                stateMachine = new AnimatorStateMachine
-                {
-                    states = new[]
-                    {
-                        defaultAnimatorState,
-                        anotherAnimatorState,
-                    },
-                    defaultState = defaultAnimatorState.state,
-                },
-
-            };
-            animatorController.AddLayer(animatorControllerLayer);
-            animator.runtimeAnimatorController = animatorController;
-
-            var tiaPlayer = tiaRoot.AddComponent<TiaPlayer>();
-            tiaPlayer.script = NewSimpleScript(testObject,
-                new TiaAnimation { AnimationName = AnotherStateName });
-
-            void AssertAnimationState(string expectedStateName) =>
-                Assert.That(
-                    animator.GetCurrentAnimatorStateInfo(0).IsName(expectedStateName),
-                    $"Expected animator state '{expectedStateName}' but was hash {animator.GetCurrentAnimatorStateInfo(0).shortNameHash}.");
-
-            AssertAnimationState(DefaultStateName);
-
-            yield return new WaitForSeconds(0.5f);
-            AssertAnimationState(AnotherStateName);
         }
 
         private class TiaAsyncStart : ITiaAction
