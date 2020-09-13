cc38e1a0e36aa2a12ff7dd17773b0df994a79952
Olli <Sjberg>
2020-09-13 14:46:01 +0300

    Create a background deer tileset

---
+++ TrappedInside/Assets/Maps/deer-background.tsx
@@ -0,0 +1,10 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.1" name="deer-background" tilewidth="16" tileheight="16" tilecount="6" columns="3">
+ <image source="deer-background.png" width="48" height="32"/>
+ <tile id="3">
+  <animation>
+   <frame tileid="0" duration="1000"/>
+   <frame tileid="3" duration="1000"/>
+  </animation>
+ </tile>
+</tileset>
