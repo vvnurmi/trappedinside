cae5479a2093506ea2320e65a77bbe68cd625940
Olli <Sjberg>
2020-11-16 21:27:43 +0200

    Update level 1 graphics and layout

---
+++ TrappedInside/Assets/Maps/mountain-2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.1" name="mountain-2" tilewidth="16" tileheight="16" tilecount="176" columns="22">
+ <image source="mountain-2.png" width="352" height="128"/>
+</tileset>
--- TrappedInside/Assets/Maps/mountain.tsx
+++ TrappedInside/Assets/Maps/mountain.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.3.1" name="mountain" tilewidth="16" tileheight="16" tilecount="128" columns="16">
- <image source="mountain.png" width="256" height="128"/>
+<tileset version="1.2" tiledversion="1.3.1" name="mountain" tilewidth="16" tileheight="16" tilecount="160" columns="20">
+ <image source="mountain.png" width="320" height="128"/>
 </tileset>
