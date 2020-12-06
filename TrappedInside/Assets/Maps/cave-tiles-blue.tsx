9d35a5721eed6d283af985d90e1dadce66addc3f
Olli <Sjberg>
2020-12-06 08:57:20 +0200

    Add Chained Calum test scene

---
+++ TrappedInside/Assets/Maps/cave-tiles-blue.tsx
@@ -0,0 +1,165 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.1" name="cave-tiles-blue" tilewidth="16" tileheight="16" tilecount="36" columns="6">
+ <image source="cave-tiles-blue.png" width="96" height="96"/>
+ <tile id="0">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+   <object id="2" x="-15" y="24.75"/>
+  </objectgroup>
+ </tile>
+ <tile id="1">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="2">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+   <object id="2" x="-2.75" y="22.375"/>
+  </objectgroup>
+ </tile>
+ <tile id="3">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+   <object id="2" x="-3.875" y="30.375"/>
+  </objectgroup>
+ </tile>
+ <tile id="4">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+   <object id="2" x="13.25" y="36.625" width="0.125"/>
+  </objectgroup>
+ </tile>
+ <tile id="5">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+   <object id="2" x="12.125" y="31.25"/>
+  </objectgroup>
+ </tile>
+ <tile id="6">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+   <object id="2" x="-8.5" y="31.125"/>
+  </objectgroup>
+ </tile>
+ <tile id="7">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+   <object id="2" x="3.25" y="29.125" width="0.125" height="0.125"/>
+  </objectgroup>
+ </tile>
+ <tile id="8">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+   <object id="2" x="-10.25" y="23.625" width="0.125" height="0.125"/>
+  </objectgroup>
+ </tile>
+ <tile id="9">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+   <object id="2" x="-7.125" y="36.75" height="0.125"/>
+  </objectgroup>
+ </tile>
+ <tile id="10">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+   <object id="2" x="-4.375" y="29.125"/>
+  </objectgroup>
+ </tile>
+ <tile id="11">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+   <object id="2" x="-2.375" y="31.5"/>
+  </objectgroup>
+ </tile>
+ <tile id="12">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+   <object id="2" x="-6.25" y="34.625" height="0.125"/>
+  </objectgroup>
+ </tile>
+ <tile id="13">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+   <object id="2" x="-7" y="34"/>
+  </objectgroup>
+ </tile>
+ <tile id="14">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+   <object id="2" x="-0.125" y="26.625"/>
+  </objectgroup>
+ </tile>
+ <tile id="15">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+   <object id="2" x="-10.75" y="31.375" width="0.25"/>
+  </objectgroup>
+ </tile>
+ <tile id="18">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="19">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+   <object id="2" x="-1.5" y="27.5"/>
+  </objectgroup>
+ </tile>
+ <tile id="20">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+   <object id="2" x="4.25" y="45.5"/>
+  </objectgroup>
+ </tile>
+ <tile id="21">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+   <object id="2" x="-11" y="35.125" height="0.125"/>
+  </objectgroup>
+ </tile>
+ <tile id="24">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="13" width="16" height="3"/>
+  </objectgroup>
+ </tile>
+ <tile id="25">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="13" width="16" height="3"/>
+  </objectgroup>
+ </tile>
+ <tile id="26">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+   <object id="2" x="-8.875" y="45.125"/>
+  </objectgroup>
+ </tile>
+ <tile id="27">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+   <object id="2" x="-5.375" y="38.375"/>
+  </objectgroup>
+ </tile>
+ <tile id="30">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="31">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+   <object id="2" x="-7.875" y="43.625"/>
+  </objectgroup>
+ </tile>
+ <tile id="32">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16" height="3"/>
+  </objectgroup>
+ </tile>
+ <tile id="33">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16" height="3"/>
+  </objectgroup>
+ </tile>
+</tileset>
---
+++ TrappedInside/Assets/Scripts/Characters/CalumMovement.cs
@@ -0,0 +1,145 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CalumMovement : MonoBehaviour
+{
+    [Tooltip("Ground collision settings.")]
+    public RaycastColliderConfig groundColliderConfig;
+
+    [Tooltip("Speed of movement along a surface, in world units per second.")]
+    public float walkingSpeed = 5.0f;
+
+    private static readonly string idle = "Idle";
+    private static readonly string walking = "Walk";
+
+    private readonly List<string> animatorStates = new List<string> { idle, walking };
+    public float gravity = -5.0f;
+
+    private CharacterState characterState;
+    private RaycastCollider groundCollider;
+    private Vector2 velocity;
+    private AttackTrigger frontAttackTrigger;
+    private AttackTrigger backAttackTrigger;
+    private HitPoints hitPoints;
+    private Animator animator;
+    private SpriteRenderer spriteRendered;
+    private Transform pole;
+    private readonly float maxDistanceFromPole = 0.4f;
+    private bool facingLeft = true;
+
+    private void Start()
+    {
+
+        characterState = GetComponent<CharacterState>();
+        var boxCollider = GetComponent<BoxCollider2D>();
+        groundCollider = new RaycastCollider(
+                groundColliderConfig,
+                boxCollider,
+                characterState.collisions);
+        velocity = Vector2.zero;
+        frontAttackTrigger = transform.GetChild(0).GetComponent<AttackTrigger>();
+        backAttackTrigger = transform.GetChild(1).GetComponent<AttackTrigger>();
+        hitPoints = GetComponent<HitPoints>();
+        animator = GetComponent<Animator>();
+        spriteRendered = GetComponent<SpriteRenderer>();
+        pole = transform.parent.GetChild(0);
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        var oldVelocity = velocity;
+        float xSpeed = oldVelocity.x;
+
+        if (frontAttackTrigger.PlayerInAttackRange && backAttackTrigger.PlayerInAttackRange)
+        {
+            SetAnimatorState(idle);
+            xSpeed = 0;
+        }
+        else if (frontAttackTrigger.PlayerInAttackRange)
+        {
+            SetAnimatorState(walking);
+            xSpeed = GetSpeed();
+        }
+        else if (backAttackTrigger.PlayerInAttackRange)
+        {
+            SetAnimatorState(walking);
+            Flip();
+            xSpeed = GetSpeed();
+        }
+        else
+        {
+            SetAnimatorState(idle);
+            xSpeed = 0;
+        }
+
+        if (facingLeft && TooFarLeft || !facingLeft && TooFarRight)
+        {
+            SetAnimatorState(idle);
+            xSpeed = 0;
+        }
+
+        velocity = new Vector2(xSpeed, oldVelocity.y + gravity * Time.deltaTime);
+        var averageVelocity = Vector2.Lerp(oldVelocity, velocity, 0.5f);
+        Move(averageVelocity * Time.deltaTime);
+
+        // Stop movement in directions where we have collided.
+        var collisions = characterState.collisions;
+        if (collisions.HasVerticalCollisions)
+            velocity.y = 0;
+        if (collisions.HasHorizontalCollisions)
+        {
+            velocity.x = 0;
+        }
+    }
+
+    private bool TooFarLeft => pole.position.x - transform.position.x > maxDistanceFromPole;
+    private bool TooFarRight => transform.position.x - pole.position.x > maxDistanceFromPole;
+
+    private float GetSpeed()
+    {
+        if (facingLeft)
+            return -walkingSpeed;
+        else
+            return walkingSpeed;
+
+    }
+
+    private void Flip()
+    {
+        facingLeft = !facingLeft;
+        transform.localScale = new Vector3(
+            -transform.localScale.x,
+            transform.localScale.y,
+            transform.localScale.z);
+    }
+
+    private void SetAnimatorState(string state)
+    {
+        ClearAnimatorState();
+        animator.SetBool(state, true);
+    }
+
+    private void ClearAnimatorState()
+    {
+        foreach (var animatorState in animatorStates)
+            animator.SetBool(animatorState, false);
+    }
+
+    private void Move(Vector2 moveAmount)
+    {
+        groundCollider.UpdateRaycastOrigins();
+
+        CollisionInfo collisions = characterState.collisions;
+        collisions.Reset();
+
+        groundCollider.HorizontalCollisions(ref moveAmount);
+        if (moveAmount.y != 0)
+            groundCollider.VerticalCollisions(ref moveAmount);
+
+        transform.Translate(moveAmount);
+    }
+
+}
