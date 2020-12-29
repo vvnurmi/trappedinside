c125c5d1b3ff312d2b1a2cd2467216823caf40b4
Ville Nurmi <ville.v.nurmi@gmail.com>
2020-12-29 15:05:27 +0200

    Working shaky text implementation

--- TrappedInside/Assets/Scripts/UI/NarrativeTypist/RichTextShaky.cs
+++ TrappedInside/Assets/Scripts/UI/NarrativeTypist/RichTextShaky.cs
@@ -7,10 +7,26 @@ using System.Text.RegularExpressions;
 using UnityEngine;
 
 /// <summary>
+/// Defines an interval of chars that's supposed to shake.
+/// <see cref="startIndex"/> and <see cref="endIndex"/> are the indices of the
+/// first char and one after the last char, in the related rich text
+/// <see cref="NarrativeTypistSetup.fullText"/>.
+/// <see cref="parms"/> defines the details of the shaking.
+/// </summary>
+internal struct ShakyCharInterval
+{
+    public int startIndex;
+    public int endIndex;
+    public ShakyTextParams parms;
+}
+
+/// <summary>
 /// Manages shaky text in a TextMeshPro rich text string.
 /// </summary>
 public class RichTextShaky
 {
+    private const int CSpaceDigits = 3;
+    private static readonly string CSpaceFormatString = "{0:N" + CSpaceDigits + "}";
     private static readonly Regex ShakyStartTagRegex = new Regex(
         @"<shaky (?:\s+ (x|y|z) = ([-0-9.]+) )* >",
         RegexOptions.Compiled | RegexOptions.CultureInvariant | RegexOptions.IgnoreCase | RegexOptions.IgnorePatternWhitespace);
@@ -23,7 +39,7 @@ public class RichTextShaky
     /// <summary>
     /// Internals exposed for automated tests.
     /// </summary>
-    internal RichTextShaky((int index, ShakyTextParams parms)[] shakyChars)
+    internal RichTextShaky(ShakyCharInterval[] shakyChars)
     {
         ShakyChars = shakyChars;
     }
@@ -34,14 +50,11 @@ public class RichTextShaky
     public bool NeedsUpdate => ShakyChars.Length > 0;
 
     /// <summary>
-    /// Pair (index, parms) of each char that's supposed to shake, where
-    /// index is the char's location in <see cref="NarrativeTypistSetup.fullText"/> and
-    /// parms defines the details of the shaking.
+    /// Intervals of shaky chars in <see cref="NarrativeTypistSetup.fullText"/>.
     /// May be empty.
     /// Internal access for automated tests.
     /// </summary>
-    // TODO !!! store intervals, because first and last chars need special treatment
-    internal (int index, ShakyTextParams parms)[] ShakyChars;
+    internal ShakyCharInterval[] ShakyChars;
 
     /// <summary>
     /// Parses shaky tags from <paramref name="richText"/>.
@@ -52,7 +65,7 @@ public class RichTextShaky
     {
         var strippedRichText = new StringBuilder(richText.Length);
         var richTextShaky = new RichTextShaky();
-        var shakyChars = new List<(int, ShakyTextParams)>();
+        var shakyChars = new List<ShakyCharInterval>();
         var index = 0;
         var skippedChars = 0;
         while (index < richText.Length)
@@ -95,15 +108,31 @@ public class RichTextShaky
             strippedRichText
                 .Append(richText, index, startMatch.Index - index)
                 .Append(richText, shakyTextStart, shakyTextLength);
-            shakyChars.AddRange(Enumerable
-                .Range(shakyTextStart - skippedChars, shakyTextLength)
-                .Select(i => (i, parms)));
+            shakyChars.Add(new ShakyCharInterval {
+                startIndex = shakyTextStart - skippedChars,
+                endIndex = shakyTextEnd - skippedChars,
+                parms = new ShakyTextParams(), // TODO !!! parse proper params from attributes
+            });
             if (endIndex != -1)
                 skippedChars += EndTag.Length;
             index = Math.Min(richText.Length, shakyTextEnd + EndTag.Length);
 
         }
         strippedRichText.Append(richText, index, richText.Length - index);
+        if (shakyChars.Count > 0)
+        {
+            Debug.Assert(shakyChars.First().startIndex > 0,
+                "Unsupported text markup: There needs to be one non-shaky char before the first shaky text interval");
+            Debug.Assert(shakyChars.Last().endIndex < richText.Length - 1,
+                "Unsupported text markup: There needs to be one non-shaky char after the last shaky text interval");
+            for (int i = 0; i < shakyChars.Count - 1; i++)
+            {
+                Debug.Assert(shakyChars[i].startIndex <= shakyChars[i].endIndex,
+                    "Shaky text internal error: Interval is backwards");
+                Debug.Assert(shakyChars[i].endIndex + 2 < shakyChars[i + 1].startIndex,
+                    "Unsupported text markup: There needs to be at least two non-shaky chars between consecutive shaky text intervals");
+            }
+        }
         richTextShaky.ShakyChars = shakyChars.ToArray();
         return (strippedRichText.ToString(), richTextShaky);
     }
@@ -140,7 +169,7 @@ public class RichTextShaky
 
             // Note: For better performance, don't format the float here but use a precalculated string array.
             result.Append("<cspace=")
-                .AppendFormat(CultureInfo.InvariantCulture, "{0:N3}", cspace)
+                .AppendFormat(CultureInfo.InvariantCulture, CSpaceFormatString, cspace)
                 .Append('>');
             AppendSafelyNoChecks(text, startIndex, endIndex);
         }
@@ -154,25 +183,29 @@ public class RichTextShaky
         // whole text block down, which is not wanted.
 
         int textProcessedUntilIndex = 0;
-        var previousOffset = 0f;
-        var lastCspacedIndex = 0;
-        foreach (var (index, parms) in ShakyChars)
+        foreach (var interval in ShakyChars)
         {
-            Debug.Assert(textProcessedUntilIndex <= index, "Shaky characters are given out of order");
-            AppendSafely(richText, textProcessedUntilIndex, index);
-            // TODO !!! <cspace={-previousOffset}> for the first char after a contiguous shaky run
-            if (index != lastCspacedIndex + 1)
-                previousOffset = 0;
-            lastCspacedIndex = index;
-            var (cspace, offset) = GetShakyCharCharacterSpacing(previousOffset, parms);
-            previousOffset = offset;
-            AppendWithCSpace(richText, index, index + 1, cspace);
-            textProcessedUntilIndex = index + 1;
+            Debug.Assert(textProcessedUntilIndex <= interval.startIndex, "Shaky characters are given out of order");
+            AppendSafely(richText, textProcessedUntilIndex, interval.startIndex - 1);
+
+            var previousOffset = 0f;
+            for (int index = interval.startIndex - 1; index < interval.endIndex - 1; index++)
+            {
+                var (cspace, offset) = GetShakyCharCharacterSpacing(previousOffset, interval.parms);
+                previousOffset = offset;
+                AppendWithCSpace(richText, index, index + 1, cspace);
+                textProcessedUntilIndex = index + 1;
+                if (textProcessedUntilIndex >= richText.Length) break;
+            }
 
             if (textProcessedUntilIndex >= richText.Length) break;
+            AppendWithCSpace(richText, interval.endIndex - 1, interval.endIndex, -previousOffset);
+            textProcessedUntilIndex++;
+            // Note: <cspace=0> is like </cspace> except that the latter would
+            // need to be placed one char further to have the exact same effect.
+            result.Append("<cspace=0>");
         }
         // The rest of the text.
-        // TODO !!! <cspace={-previousOffset}> for the first remaining char
         AppendSafely(richText, textProcessedUntilIndex, richText.Length);
 
         return result.ToString();
@@ -183,7 +216,9 @@ public class RichTextShaky
         ShakyTextParams parms)
     {
         var amplitude = 0.005f;
-        var charOffset = amplitude * (1 - 2 * (float)RandomNumber.NextDouble());
-        return (charOffset - previousOffset, charOffset);
+        var charOffset = amplitude * (1 - 2 * RandomNumber.NextDouble());
+        // Round the offset to the precision that's used in the rich text attribute.
+        var roundedCharOffset = (float)Math.Round(charOffset, CSpaceDigits);
+        return (roundedCharOffset - previousOffset, roundedCharOffset);
     }
 }
