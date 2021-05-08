5ee78436f18ea4017b563a23b8134fdcae5e735e
Ville Nurmi <ville.v.nurmi@gmail.com>
2021-05-08 21:49:21 +0300

    PressurePlate prefab

---
+++ TrappedInside/Assets/Scripts/Mechanisms/PressurePlateController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+/// <summary>
+/// Public interface and main implementation of the PressurePlate prefab.
+/// </summary>
+public class PressurePlateController : MonoBehaviour
+{
+    /// <summary>
+    /// Called when the pressure plate is triggered.
+    /// </summary>
+    public UnityEvent onTriggered;
+
+    #region MonoBehaviour overrides
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        onTriggered?.Invoke();
+    }
+
+    #endregion
+}
