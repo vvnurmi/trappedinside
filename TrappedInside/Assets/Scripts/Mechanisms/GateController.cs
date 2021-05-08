695f4a29c6b2e8fb0b93c7b4a248ae06ea968219
Ville Nurmi <ville.v.nurmi@gmail.com>
2021-05-08 21:49:03 +0300

    Gate prefab

---
+++ TrappedInside/Assets/Scripts/Mechanisms/GateController.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Public interface and main implementation of the Gate prefab.
+/// </summary>
+public class GateController : MonoBehaviour
+{
+    private Rigidbody2D blocker;
+
+    public bool IsOpen => blocker?.simulated == false;
+    public bool IsClosed => blocker?.simulated == true;
+
+    public void Open()
+    {
+        if (IsOpen) return;
+
+        Debug.Log($"Opening gate {this.GetFullName()}");
+        blocker.simulated = false;
+    }
+
+    public void Close()
+    {
+        if (IsClosed) return;
+
+        Debug.Log($"Closing gate {this.GetFullName()}");
+        blocker.simulated = true;
+    }
+
+    #region MonoBehaviour overrides
+
+    public void Start()
+    {
+        blocker = GetComponentInChildren<Rigidbody2D>();
+        Debug.Assert(blocker != null);
+    }
+
+    #endregion
+}
