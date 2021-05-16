34b7e3cc15af5c31f0cde73f833876b3152fea92
Ville Nurmi <ville.v.nurmi@gmail.com>
2021-05-16 21:52:45 +0300

    Pressure plate release has an event, too

--- TrappedInside/Assets/Scripts/Mechanisms/PressurePlateController.cs
+++ TrappedInside/Assets/Scripts/Mechanisms/PressurePlateController.cs
@@ -11,11 +11,21 @@ public class PressurePlateController : MonoBehaviour
     /// </summary>
     public UnityEvent onTriggered;
 
+    /// <summary>
+    /// Called when the pressure plate is released.
+    /// </summary>
+    public UnityEvent onReleased;
+
     #region MonoBehaviour overrides
 
     public void OnTriggerEnter2D(Collider2D collision)
     {
         onTriggered?.Invoke();
+    }
+
+    public void OnTriggerExit2D(Collider2D collision)
+    {
+        onReleased?.Invoke();
     }
 
     #endregion
