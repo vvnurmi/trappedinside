b398880f1e0864ae8f2835d28ac7b627355039b1
Ville Nurmi <ville.v.nurmi@gmail.com>
2021-06-30 12:10:51 +0300

    Extracted Play(TiaScript) from TiaPlayer.Start()

--- TrappedInside/Assets/Scripts/Tools/TIA/TiaPlayer.cs
+++ TrappedInside/Assets/Scripts/Tools/TIA/TiaPlayer.cs
@@ -14,20 +14,32 @@ public class TiaPlayer : MonoBehaviour
     private TiaActionContext context;
     private int stepIndex;
 
+    /// <summary>
+    /// Starts to play <paramref name="script"/>.
+    /// If another script is already playing, it's abandoned.
+    /// </summary>
+    /// <param name="pauseImmediately">If true, playback will remain paused
+    /// until <see cref="IsPlaying"/> is set.</param>
+    public void Play(TiaScript script, bool pauseImmediately = false)
+    {
+        this.script = script;
+
+        context = new TiaActionContext(
+            scriptRunner: this,
+            tiaRoot: gameObject);
+        IsPlaying = !pauseImmediately;
+        stepIndex = 0;
+        if (stepIndex < script.Steps.Length)
+            script.Steps[stepIndex].Start(context);
+    }
+
     #region MonoBehaviour overrides
 
     private async void Start()
     {
         Debug.Assert(script != null || !string.IsNullOrEmpty(scriptName), $"No script assigned to {nameof(TiaPlayer)}");
-        script = script ?? await TiaScriptManager.Instance.Get(scriptName);
-
-        context = new TiaActionContext(
-            scriptRunner: this,
-            tiaRoot: gameObject);
-        IsPlaying = script.PlayOnStart;
-        stepIndex = 0;
-        if (stepIndex < script.Steps.Length)
-            script.Steps[stepIndex].Start(context);
+        var scriptToPlay = script ?? await TiaScriptManager.Instance.Get(scriptName);
+        Play(scriptToPlay, pauseImmediately: !script.PlayOnStart);
     }
 
     private void Update()
