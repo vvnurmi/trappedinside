6dfab35fc3bee148e9626caa88af8c3b3b05812f
Ville Nurmi <ville.v.nurmi@gmail.com>
2021-06-30 12:14:49 +0300

    Added comments

--- TrappedInside/Assets/Scripts/Tools/TIA/TiaDebug.cs
+++ TrappedInside/Assets/Scripts/Tools/TIA/TiaDebug.cs
@@ -2,6 +2,11 @@
 
 public static class TiaDebug
 {
+    /// <summary>
+    /// Log a message with TIA tag.
+    /// Good for grouping TIA log messages and also to
+    /// conditionally exclude TIA logs from compilation.
+    /// </summary>
     [System.Diagnostics.Conditional("TIA_DEBUG")]
     public static void Log(string message) =>
         Debug.unityLogger.Log("TIA", message);
--- TrappedInside/Assets/Scripts/Tools/TIA/TiaMethods.cs
+++ TrappedInside/Assets/Scripts/Tools/TIA/TiaMethods.cs
@@ -3,9 +3,15 @@
     public static bool testFlag;
     public static void SetTestFlagToTrue() => testFlag = true;
 
+    /// <summary>
+    /// Disables controls of the player character.
+    /// </summary>
     public static void DisablePlayerInput() =>
         TIInputStateManager.instance.DisablePlayerInput();
 
+    /// <summary>
+    /// Enables controls of the player character.
+    /// </summary>
     public static void EnablePlayerInput() =>
         TIInputStateManager.instance.EnablePlayerInput();
 }
