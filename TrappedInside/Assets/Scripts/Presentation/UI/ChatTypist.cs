c8c9b95cd88f41fa694c619e490f5951002d4717
Olli <olli.a.sjoberg@gmail.com>
2021-07-13 12:30:04 +0300

    Add blue screen to the end of bee boss fight
    Contains an initial implementation of the blue screen prefab

---
+++ TrappedInside/Assets/Scripts/Presentation/UI/ChatTypist.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class ChatTypist : MonoBehaviour
+{
+    public float lineInterval;
+    public string[] lines;
+
+    private Queue<string> _lines = new Queue<string>();
+    private float _previousTypingTime;
+    private TMPro.TextMeshProUGUI _textField;
+
+    void Start()
+    {
+        _previousTypingTime = Time.time;
+        foreach(var line in lines)
+            _lines.Enqueue(line);
+    }
+
+    void Awake()
+    {
+        _textField = GetComponentsInChildren<TMPro.TextMeshProUGUI>(includeInactive: true)[0];
+        Debug.Assert(_textField != null);
+    }
+
+    void FixedUpdate()
+    {
+        if(Time.time < _previousTypingTime + lineInterval) return;
+
+        if(_lines.Count > 0)
+            _textField.text += $"{_lines.Dequeue()}\n";
+
+        _previousTypingTime = Time.time;
+    }
+
+}
