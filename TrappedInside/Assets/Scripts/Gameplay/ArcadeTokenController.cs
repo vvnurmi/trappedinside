3f09597f805beec9cb384f01faf295dcab0b3840
Olli <olli.a.sjoberg@gmail.com>
2021-07-15 13:30:34 +0300

    Add arcade token bling animation after it is collected.

---
+++ TrappedInside/Assets/Scripts/Gameplay/ArcadeTokenController.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class ArcadeTokenController : MonoBehaviour
+{
+    public GameObject arcadeTokenBlingPrefab;
+
+    void OnDisable()
+    {
+        Instantiate(arcadeTokenBlingPrefab, transform.position, Quaternion.identity);
+    }
+}
