40ea8c4259b678c2249517c80f57ec53f768c892
Olli <olli.a.sjoberg@gmail.com>
2021-07-16 22:44:26 +0300

    Add business card end animation

--- TrappedInside/Assets/BusinessCardController.cs
+++ TrappedInside/Assets/BusinessCardController.cs
@@ -2,9 +2,12 @@
 
 public class BusinessCardController : MonoBehaviour, ICollectible
 {
+    public GameObject businessCardEndPrefab;
+
     public void Collect(CharacterState characterState)
     {
         characterState.collectedBusinessCards++;
+        Instantiate(businessCardEndPrefab, transform.position, Quaternion.identity);
         gameObject.SetActive(false);
     }
 }
---
+++ TrappedInside/Assets/Scripts/Gameplay/ObjectDestroy.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public class ObjectDestroy : MonoBehaviour
+{
+    void Destroy()
+    {
+        Destroy(gameObject);
+    }    
+}
