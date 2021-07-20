b7faa767a6bb8f2835142a2e428603ffa86be54a
Olli <olli.a.sjoberg@gmail.com>
2021-07-20 13:52:32 +0300

    Improve boss light sequence
    * Set object colors instead of activating elements
    * Light up background before foreground.

---
+++ TrappedInside/Assets/Scripts/Presentation/Visuals/ColorSetter.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class ColorSetter : MonoBehaviour
+{
+    public Color color;
+    public GameObject coloredObject;
+
+    void Start()
+    {
+        var spriteRenderer = coloredObject.GetComponent<SpriteRenderer>();
+        spriteRenderer.color = color;
+    }
+}
